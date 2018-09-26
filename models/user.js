6a6d2e5734a9c36e22e29526ce4bd28ae445235e
Shannan Galbraith <s.galbraith08@gmail.com>
2018-09-26 22:35:18 +1000

    updated user model

--- models/user.js
+++ models/user.js
@@ -1,170 +1,213 @@
 "use strict";
-// module.exports = (sequelize, DataTypes) => {
-//   const user = sequelize.define(
-//     "user",
-//     {
-//       username: DataTypes.STRING,
-//       description: DataTypes.TEXT,
-//       colorIcon: DataTypes.STRING,
-//       password: DataTypes.STRING,
-//       email: DataTypes.STRING,
-//       RoleId: DataTypes.INTEGER,
-//       VillageId: DataTypes.INTEGER,
-//       PlayerId: DataTypes.INTEGER,
-//       points: DataTypes.INTEGER,
-//       facebookToken: DataTypes.STRING,
-//       googleToken: DataTypes.STRING,
-//       FriendId: DataTypes.INTEGER,
-//       avatar: DataTypes.STRING,
-//       IpId: DataTypes.INTEGER
-//     },
-//     {}
-//   );
-//   user.associate = function(models) {
-//     // associations can be defined here
-//   };
-//   return user;
-// };
 
-const Sequelize = require("sequelize");
 const randomColor = require("randomcolor");
+const bcrypt = require("bcryptjs");
+const pagination = require("../../helpers/pagination");
+const errors = require("../../helpers/mainErrors");
 
-module.exports = class User extends Sequelize.Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        username: {
-          type: DataTypes.STRING,
-          validate: {
-            notEmpty: true,
-            isString(val) {
-              if (typeof val !== "string") {
-                throw new sequelize.ValidationError(
-                  "Username must be a string."
-                );
-              }
-            },
-            checkBlankChars(val) {
-              if (/\s/g.test(val)) {
-                throw new sequelize.ValidationError(
-                  "Username cannot contain blank characters."
-                );
-              }
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      username: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: true,
+          isString(val) {
+            if (typeof val !== "string") {
+              throw new sequelize.ValidationError("Username must be a string.");
             }
-          }
-        },
-        description: {
-          type: DataTypes.TEXT,
-          validate: {
-            len: {
-              args: [1, 1024],
-              msg: "Description must be between 1 and 1024 characters."
-            },
-            isString(val) {
-              if (typeof val !== "string") {
-                throw new sequelize.ValidationError(
-                  "Username must be a string."
-                );
-              }
-            }
-          }
-        },
-        colorIcon: {
-          type: DataTypes.STRING,
-          defaultValue() {
-            return randomColor();
-          }
-        },
-        password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len: {
-              args: [6, 100],
-              msg: "Password must be between 6 and 100 characters"
-            },
-            isString(val) {
-              if (typeof val !== "string") {
-                throw new sequelize.ValidationError(
-                  "Username must be a string."
-                );
-              }
-            }
-          }
-        },
-        email: {
-          type: DataTypes.STRING,
-          unique: { msg: "Email is already registered.", fields: ["email"] },
-          allowNull: false,
-          validate: {
-            isEmail: true,
-            notEmpty: true,
-            len: {
-              args: [1, 100],
-              msg: "Email must be between 1 and 100 characters"
-            },
-            isString(val) {
-              if (typeof val !== "string") {
-                throw new sequelize.ValidationError("Email must be a string");
-              }
-            }
-          }
-        },
-        // RoleId: { type: DataTypes.INTEGER },
-        // VillageId: { type: DataTypes.INTEGER },
-        // PlayerId: { type: DataTypes.INTEGER },
-        points: {
-          type: DataTypes.INTEGER,
-          validation: {
-            isInt: true
-          }
-        },
-        facebookToken: {
-          type: DataTypes.STRING,
-          validate: {
-            isString(val) {
-              if (typeof val !== "string") {
-                throw new sequelize.ValidationError(
-                  "Facebook token must be a string."
-                );
-              }
-            }
-          }
-        },
-        googleToken: {
-          type: DataTypes.STRING,
-          validate: {
-            isString(val) {
-              if (typeof val !== "string") {
-                throw new sequelize.ValidationError(
-                  "Google token must be a string."
-                );
-              }
-            }
-          }
-        },
-        // FriendId: { type: DataTypes.INTEGER },
-        avatar: {
-          type: DataTypes.STRING,
-          validate: {
-            isString(val) {
-              if (typeof val !== "string") {
-                throw new sequelize.ValidationError("Avatar must be a string.");
-              }
+          },
+          checkBlankChars(val) {
+            if (/\s/g.test(val)) {
+              throw new sequelize.ValidationError(
+                "Username cannot contain blank characters."
+              );
             }
           }
         }
-        // IpId: { type: DataTypes.INTEGER }
       },
-      {
-        sequelize
+      description: {
+        type: DataTypes.TEXT,
+        validate: {
+          len: {
+            args: [1, 1024],
+            msg: "Description must be between 1 and 1024 characters."
+          },
+          isString(val) {
+            if (typeof val !== "string") {
+              throw new sequelize.ValidationError("Username must be a string.");
+            }
+          }
+        }
+      },
+      colorIcon: {
+        type: DataTypes.STRING,
+        defaultValue() {
+          return randomColor();
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [6, 100],
+            msg: "Password must be between 6 and 100 characters"
+          },
+          isString(val) {
+            if (typeof val !== "string") {
+              throw new sequelize.ValidationError("Username must be a string.");
+            }
+          }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: { msg: "Email is already registered.", fields: ["email"] },
+        allowNull: false,
+        validate: {
+          isEmail: true,
+          notEmpty: true,
+          len: {
+            args: [1, 100],
+            msg: "Email must be between 1 and 100 characters"
+          },
+          isString(val) {
+            if (typeof val !== "string") {
+              throw new sequelize.ValidationError("Email must be a string");
+            }
+          }
+        }
+      },
+      // RoleId: { type: DataTypes.INTEGER },
+      // VillageId: { type: DataTypes.INTEGER },
+      // PlayerId: { type: DataTypes.INTEGER },
+      points: {
+        type: DataTypes.INTEGER,
+        validation: {
+          isInt: true
+        }
+      },
+      facebookToken: {
+        type: DataTypes.STRING,
+        validate: {
+          isString(val) {
+            if (typeof val !== "string") {
+              throw new sequelize.ValidationError(
+                "Facebook token must be a string."
+              );
+            }
+          }
+        }
+      },
+      googleToken: {
+        type: DataTypes.STRING,
+        validate: {
+          isString(val) {
+            if (typeof val !== "string") {
+              throw new sequelize.ValidationError(
+                "Google token must be a string."
+              );
+            }
+          }
+        }
+      },
+      // FriendId: { type: DataTypes.INTEGER },
+      avatar: {
+        type: DataTypes.STRING,
+        validate: {
+          isString(val) {
+            if (typeof val !== "string") {
+              throw new sequelize.ValidationError("Avatar must be a string.");
+            }
+          }
+        }
       }
-    );
+      // IpId: { type: DataTypes.INTEGER }
+    },
+    {}
+  );
+
+  User.associate = function(models) {
+    User.hasMany(models.Post);
+    User.hasMany(models.Thread);
+    User.belongsToMany(models.Ip, { through: "UserIp" });
+    User.hasOne(models.Village)
+    User.belongsTo(models.Role, { through: 'userrole'})
+  };
+
+
+  User.prototype.updatePassword = function(sentPassword, newPassword) {
+    // check to make sure the user isnt using the previous password
+    if (sentPassword === newPassword) {
+      throw errors.passwordsAreTheSame;
+    }
+
+    // authenticate the password before updating
+    const checkPassword = await bcrypt.compare(sentPassword, this.password);
+
+    // if everything checks out do the update
+    if (checkPassword) {
+      await this.update({ password: newPassword });
+    } else {
+      throw errors.notAuthenticated;
+    }
   }
 
-  static associate(models) {
-    this.hasMany(models.Post);
-    this.hasMany(models.Thread);
-    this.belongsToMany(models.Ip, { through: "UserIp" });
+  // compare passwords for use with logging in etc
+  User.prototype.validPassword = async function(sentPassword) {
+    return await bcrypt.compare(sentPassword, this.password);
   }
+
+  User.prototype.getUsersMetaData = function (limit) {
+    const Post = sequelize.Models.Post;
+    let metaData = {};
+
+    // get the id of the next post
+    const nextPostId = await pagination.getNextIdOrderedByDesc(
+      Post,
+      { UserId: this.id },
+      this.Post
+    );
+
+    // build the required url
+    if (nextPostId === null) {
+      metaData.URL = null;
+      metaData.PostCount = 0;
+    } else {
+      metaData.URL = trim(
+        `/api/v1/${this.username}?posts=true&limit=${limit}&from=${nextPostId -
+        1}`
+      );
+      metaData.PostCount = await pagination.getTotalPostCount(
+        Post,
+        this.Post,
+        limit,
+        { UserId: this.id },
+        true
+      );
+    }
+
+    return metaData;
+  }
+
+  // return users posts
+  User.userOptions = function(from, limit) {
+    const {Post, Village} = sequelize.models;
+
+    return [
+      {
+        model: models.Post,
+        include: Post.postOptions,
+        limit,
+        where: { postNumber: { $gte: from } },
+        order: [["id", "ASC"]]
+      },
+      Village
+    ];
+  }
+
+
+  return User;
 };
