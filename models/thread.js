fd3859c36da9bd5b6b3d8d4c7775cfc729482653
Shannan Galbraith <s.galbraith08@gmail.com>
2018-09-26 22:34:28 +1000

    updated thread

--- models/thread.js
+++ models/thread.js
@@ -1,18 +1,224 @@
-'use strict';
+"use strict";
+const slug = require("slugify");
+const randomColor = require("randomcolor");
+const moment = require("moment");
+const marked = require("../helpers/markedSetup");
+const errors = require("../helpers/mainErrors");
+
 module.exports = (sequelize, DataTypes) => {
-  const thread = sequelize.define('thread', {
-    title: DataTypes.STRING,
-    slug: DataTypes.STRING,
-    postCount: DataTypes.INTEGER,
-    locaked: DataTypes.BOOLEAN,
-    CategoryId: DataTypes.INTEGER,
-    UserId: DataTypes.INTEGER,
-    PollQueryId: DataTypes.INTEGER,
-    titleBGColor: DataTypes.STRING,
-    discussion: DataTypes.TEXT
-  }, {});
-  thread.associate = function(models) {
-    // associations can be defined here
+  const Thread = sequelize.define(
+    "Thread",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        set(val) {
+          this.setDataValue("title", val);
+          if (val) {
+            this.setDataValue("slug", slug(val).toLowerCase());
+          }
+        },
+        validate: {
+          notEmpty: {
+            msg: "The title cannot be empty."
+          },
+          len: {
+            args: [3, 240],
+            msg: "The title must be between 3 and 240 characters."
+          },
+          isString(val) {
+            if (typeof val !== "string") {
+              throw new sequelize.ValidationError(
+                "The title must be a string."
+              );
+            }
+          }
+        }
+      },
+      slug: { type: DataTypes.STRING },
+      postCount: { type: DataTypes.INTEGER, defaultValue: 0 },
+      locked: { type: DataTypes.BOOLEAN, defaultValue: false },
+      CategoryId: { type: DataTypes.INTEGER },
+      UserId: { type: DataTypes.INTEGER },
+      PollQueryId: { type: DataTypes.INTEGER },
+      movedTo: { type: DataTypes.INTEGER },
+      isSticky: { type: DataTypes.BOOLEAN, defaultValue: false },
+      stickyDuration: { type: DataTypes.DATE },
+      threadPosition: { type: DataTypes.INTEGER },
+      SubscriptionId: {
+        type: DataTypes.INTEGER
+      },
+      LikeId: {
+        type: DataTypes.INTEGER
+      },
+      titleBGColor: {
+        type: DataTypes.STRING,
+        defaultValue() {
+          return randomColor();
+        }
+      },
+      discussion: {
+        type: DataTypes.TEXT,
+        set(val) {
+          if (!val) {
+            throw new sequelize.ValidationError(sequelize, {
+              error: "discussion must be a string.",
+              field: "discussion"
+            });
+          }
+          this.setDataValue("discussion", marked(val)); // set up with editor options later
+        },
+        allowNull: false
+      }
+    },
+    {}
+  );
+
+  // class methods
+
+  Thread.associate = function(models) {
+    Thread.belongsTo(models.User);
+    Thread.belongsTo(models.Category);
+    Thread.belongsTo(models.PollQuery);
+    Thread.hasMany(models.Subscription, { foreignKeyConstraint: true });
+    Thread.hasMany(models.Post, {
+      foreignKeyConstraint: true,
+      onDelete: "CASCADE"
+    });
+    Thread.hasMany(models.Like, { foreignKeyConstraint: true });
   };
-  return thread;
+
+  // get other related thread data to return
+  Thread.threadOptions = function(from, limit) {
+    const { User, Category, Post, Thread, Villlage } = sequelize.models;
+
+    return [
+      { model: User, attributes: ["id", "username", "color", "avatar"] },
+      Category,
+      {
+        model: Post,
+        where: { postPosition: { $gte: from } },
+        order: [["id", "ASC"]],
+        limit,
+        include: [
+          { model: Thread, attributes: ["slug"] },
+          { model: Like, where: { ThreadId: Thread.id } },
+          { model: User, attributes: ["id", "username", "color", "avatar"] },
+          {
+            model: Post,
+            as: "Replies",
+            include: [
+              {
+                User,
+                attributes: ["id", "username", "color", "avatar"]
+              }
+            ]
+          },
+          Village
+        ]
+      }
+    ];
+  };
+
+  // instance methods
+
+  Thread.prototype.getMetaData = function(limit) {
+    let meta = {};
+
+    const posts = this.Post;
+    const firstPost = posts[0];
+    const lastPost = posts.slice(-1)[0];
+
+    // get the next url for user threads
+    if (!lastPost || lastPost.postPosition + 1 === this.postCount) {
+      meta.nextURL = null;
+    } else {
+      meta.URL = trim(
+        `api/v1/thread/${this.id}?limit=${limit}&from=${lastPost.postPosition +
+          1}`
+      );
+    }
+
+    // get the previous url
+    if (!firstPost || firstPost.postPosition === 0) {
+      meta.prevURL = trim(
+        `/api/v1/thread/${this.id}?limit=${firstPost.postPosition}&from=0`
+      );
+    } else {
+      meta.prevURL = trim(
+        `/api/v1/thread/${
+          this.id
+        }?limit=${limit}&from=${firstPost.postPosition - limit}`
+      );
+    }
+
+    // get the post counts
+    if (lastPost === undefined) {
+      meta.nextPostCount = 0;
+      meta.prevPostCount = 0;
+      meta.remainingPosts = 0;
+    } else {
+      const remaining = this.postPosition - lastPost.postPosition - 1;
+
+      meta.remainingPosts = remaining;
+
+      if (remaining < limit) {
+        meta.nextPostCount = remaining;
+      } else {
+        meta.nextPostCount = limit;
+      }
+
+      if (firstPost.postPosition === 0) {
+        meta.prevPostCount = 0;
+      } else if (firstPost.postPosition - limit < 0) {
+        meta.prevPostCount = firstPost.postPosition;
+      } else {
+        meta.prevPostCount = limit;
+      }
+    }
+  };
+
+  Thread.prototype.moveThisThreadTo = async function(id, newCategory) {
+    const { Thread, Category } = sequelize.models;
+    const currentCategory = await Category.findByPrimary(this.CategoryId);
+    const allCategories = await Category.findAll();
+
+    if (id === currentCategory.id) {
+      throw errors.parameterError(
+        "CategoryId",
+        "You cannot move this thread to the same category."
+      );
+    } else if (!allCategories) {
+      throw errors.categoryError;
+    } else {
+      Thread.update({
+        CategoryId: id
+      });
+    }
+  };
+
+  Thread.prototype.markAsSticky = async function(id) {
+    const thread = await Thread.findById(id);
+    const currentThreadPosition = thread.threadPosition;
+
+    if (!thread) {
+      throw errors.parameterError("threadId", "That thread does not exist.");
+    }
+
+    thread.unshift(currentThreadPosition);
+
+    thread.reload();
+  };
+
+  Thread.prototype.checkStickyDuration = async function() {
+    const expires = this.stickyDuration;
+
+    if (moment().isAfter(expires)) {
+      this.update({
+        isSticky: false
+      });
+    }
+  };
+
+  return Thread;
 };
