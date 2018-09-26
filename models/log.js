e0208b4f36dc88f481143547161cc589731c5e0a
Shannan Galbraith <s.galbraith08@gmail.com>
2018-09-26 22:29:31 +1000

    updated log model

--- models/log.js
+++ models/log.js
@@ -1,15 +1,32 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const log = sequelize.define('log', {
-    UserId: DataTypes.INTEGER,
-    ThreadId: DataTypes.INTEGER,
-    SessionId: DataTypes.INTEGER,
-    resource: DataTypes.STRING,
-    method: DataTypes.STRING,
-    status: DataTypes.INTEGER
-  }, {});
-  log.associate = function(models) {
-    // associations can be defined here
+  const Log = sequelize.define(
+    "Log",
+    {
+      UserId: DataTypes.INTEGER,
+      ThreadId: DataTypes.INTEGER,
+      SessionId: DataTypes.INTEGER,
+      resource: DataTypes.STRING,
+      method: DataTypes.STRING,
+      status: DataTypes.INTEGER
+    },
+    {}
+  );
+  Log.associate = function(models) {
+    Log.belongsTo(models.Thread);
+    Log.belongsTo(models.User);
+    Log.belongsTo(models.User, { as: "SessionUser" });
+    Log.belongsTo(models.Like);
+    Log.belongsTo(models.Ban);
+    Log.belongsTo(models.PenaltyBox);
+    Log.belongsTo(models.Post);
+    Log.belongsTo(models.Category);
+    Log.belongsTo(models.PollQuery);
+    Log.belongsTo(models.PollBallot);
+    Log.belongsTo(models.Report);
+    Log.belongsTo(models.Search);
+    Log.belongsTo(models.Setting);
+    Log.belongsTo(models.Subscription);
   };
-  return log;
+  return Log;
 };
