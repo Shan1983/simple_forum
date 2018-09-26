3c1ee92a9fd54fb41b6d0f089691681667bfdb0d
Shannan Galbraith <s.galbraith08@gmail.com>
2018-09-26 22:26:53 +1000

    completed category model

--- models/category.js
+++ models/category.js
@@ -1,12 +1,55 @@
-'use strict';
+"use strict";
+const randomColor = require("randomcolor");
+const slug = require("slugify");
+
 module.exports = (sequelize, DataTypes) => {
-  const category = sequelize.define('category', {
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    colorIcon: DataTypes.STRING
-  }, {});
-  category.associate = function(models) {
-    // associations can be defined here
+  const Category = sequelize.define(
+    "Category",
+    {
+      title: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Category must have a name."
+          },
+          isString(val) {
+            if (typeof val !== "string") {
+              throw new sequelize.ValidationError(
+                "The category must be a string"
+              );
+            }
+          }
+        }
+      },
+      description: {
+        type: DataTypes.TEXT
+      },
+      colorIcon: {
+        type: DataTypes.STRING,
+        defaultValue() {
+          return randomColor();
+        }
+      }
+    },
+    {}
+  );
+
+  // class methods
+
+  Category.associate = function(models) {
+    Category.hasMany(models.Threads);
   };
-  return category;
+
+  Category.hook("beforeCreate", category => {
+    if (!category.title) {
+      throw new sequelize.ValidationError("The category must have a title");
+    } else {
+      // slugify the category title
+      const title = slug(category.title).toUpperCase();
+      category.title = title;
+    }
+  });
+  return Category;
 };
