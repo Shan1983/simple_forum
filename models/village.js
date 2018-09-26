2af991d3ef34c7a7e33dfa4b859d5cab238dd076
Shannan Galbraith <s.galbraith08@gmail.com>
2018-09-26 22:37:30 +1000

    updated village model and add moment

--- models/village.js
+++ models/village.js
@@ -1,28 +1,34 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const village = sequelize.define('village', {
-    townhallLevel: DataTypes.INTEGER,
-    TroopsId: DataTypes.INTEGER,
-    league: DataTypes.INTEGER,
-    builderTownhallLevel: DataTypes.INTEGER,
-    playerTag: DataTypes.STRING,
-    expLevel: DataTypes.INTEGER,
-    leagueIcon: DataTypes.STRING,
-    trophies: DataTypes.INTEGER,
-    versusTrophies: DataTypes.INTEGER,
-    attackWins: DataTypes.INTEGER,
-    defenceWins: DataTypes.INTEGER,
-    clanTag: DataTypes.STRING,
-    clanBadgeUrl: DataTypes.STRING,
-    donations: DataTypes.INTEGER,
-    donationsReceived: DataTypes.INTEGER,
-    warStars: DataTypes.INTEGER,
-    clanRole: DataTypes.STRING,
-    SpellsId: DataTypes.INTEGER,
-    HeroesId: DataTypes.INTEGER
-  }, {});
-  village.associate = function(models) {
-    // associations can be defined here
+  const Village = sequelize.define(
+    "Village",
+    {
+      townhallLevel: DataTypes.INTEGER,
+      TroopsId: DataTypes.INTEGER,
+      league: DataTypes.INTEGER,
+      builderTownhallLevel: DataTypes.INTEGER,
+      playerTag: DataTypes.STRING,
+      expLevel: DataTypes.INTEGER,
+      leagueIcon: DataTypes.STRING,
+      trophies: DataTypes.INTEGER,
+      versusTrophies: DataTypes.INTEGER,
+      attackWins: DataTypes.INTEGER,
+      defenceWins: DataTypes.INTEGER,
+      clanTag: DataTypes.STRING,
+      clanBadgeUrl: DataTypes.STRING,
+      donations: DataTypes.INTEGER,
+      donationsReceived: DataTypes.INTEGER,
+      warStars: DataTypes.INTEGER,
+      clanRole: DataTypes.STRING,
+      SpellsId: DataTypes.INTEGER,
+      HeroesId: DataTypes.INTEGER
+    },
+    {}
+  );
+  Village.associate = function(models) {
+    Village.hasOne(models.Troop);
+    Village.hasOne(models.Heroes);
+    Village.hasOne(models.Spell);
   };
-  return village;
+  return Village;
 };
