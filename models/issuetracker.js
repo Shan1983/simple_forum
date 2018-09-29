47d4bee8126398c1ce729f69483b46746063c142
Shannan Galbraith <s.galbraith08@gmail.com>
2018-09-29 22:43:12 +1000

    updated isse tracker model

--- models/issuetracker.js
+++ models/issuetracker.js
@@ -1,14 +1,58 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const issuetracker = sequelize.define('issuetracker', {
-    issue: DataTypes.STRING,
-    severity: DataTypes.ENUM,
-    notes: DataTypes.TEXT,
-    PriorityId: DataTypes.INTEGER,
-    UserId: DataTypes.INTEGER
-  }, {});
-  issuetracker.associate = function(models) {
-    // associations can be defined here
+  const Issuetracker = sequelize.define(
+    "Issuetracker",
+    {
+      issue: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [1, 500],
+            msg: "Isee must be between 1 and 500 characters"
+          },
+          isString(val) {
+            if (typeof val !== "string") {
+              throw sequelize.ValidationError("The issue must be a string.");
+            }
+          }
+        }
+      },
+      severity: {
+        type: DataTypes.ENUM,
+        values: ["LOW", "MODERATE", "HIGH", "URGENT"]
+      },
+      notes: {
+        type: DataTypes.TEXT,
+        validate: {
+          len: {
+            args: [1, 500],
+            msg: "Notes must be between 1 and 500 characters"
+          },
+          isString(val) {
+            if (typeof val !== "string") {
+              throw sequelize.ValidationError("Notes must be a string");
+            }
+          }
+        }
+      },
+      PriorityId: { type: DataTypes.INTEGER },
+      UserId: { type: DataTypes.INTEGER },
+      points: { type: DataTypes.INTEGER, defaultValue: 0 }
+    },
+    {}
+  );
+
+  // class Methods
+
+  Issuetracker.associate = function(models) {
+    Issuetracker.belongsTo(models.User);
   };
-  return issuetracker;
+
+  // instance Methods
+  Issuetracker.prototype.upVote = async function() {
+    await this.points.increment("points", { by: 1 });
+    return true;
+  };
+
+  return Issuetracker;
 };
