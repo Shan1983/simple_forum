0bafbac9ff4eb45c80391a34670ac4410976d29d
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-01 00:40:55 +1000

    fixed a typo

--- models/role.js
+++ models/role.js
@@ -78,101 +78,102 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   // promote a new admin user
-  Role.prototype.promoteToAdmin = function(id) {
-    const {Role, User} = sequelize.models
+  Role.prototype.promoteToAdmin = async function(id) {
+    const { Role, User } = sequelize.models;
 
-    const user = await User.findById(id)
+    const user = await User.findById(id);
 
-    if(!user) {
+    if (!user) {
       throw errors.sequelizeValidation(sequelize, {
-        errors: 'User does not exist.',
+        errors: "User does not exist.",
         userId: id
-      })
+      });
     }
 
     // check users current role
-    if(user.Role.role === 'Administrator') {
-      throw errors.parameterError('role','This user is already an administrator.')
-    } else if (user.Role.role === 'System') {
-      throw errors.parameterError('role','This user cannot be changed.')
+    if (user.Role.role === "Administrator") {
+      throw errors.parameterError(
+        "role",
+        "This user is already an administrator."
+      );
+    } else if (user.Role.role === "System") {
+      throw errors.parameterError("role", "This user cannot be changed.");
     }
 
     // promote the user to admin
-    await Role.setUserRole(user)
-    await Role.setRole('Administrator')
+    await Role.setUserRole(user);
+    await Role.setRole("Administrator");
 
     const roleReload = Role.reload({
-      include:[
-        {model: Role, attributes: ['role']},
-        {model: User, attributes: ['id','username','color','avatar']}
+      include: [
+        { model: Role, attributes: ["role"] },
+        { model: User, attributes: ["id", "username", "color", "avatar"] }
       ]
-    })
+    });
 
     return roleReload;
-
-  }
+  };
 
   // promote a new moderator
-  Role.prototype.promoteToModerator = function(id) {
-    const { Role, User } = sequelize.models
+  Role.prototype.promoteToModerator = async function(id) {
+    const { Role, User } = sequelize.models;
 
-    const user = await User.findById(id)
+    const user = await User.findById(id);
 
     if (!user) {
       throw errors.sequelizeValidation(sequelize, {
-        errors: 'User does not exist.',
+        errors: "User does not exist.",
         userId: id
-      })
+      });
     }
 
     // check users current role
-    if (user.Role.role === 'Moderator') {
-      throw errors.parameterError('role', 'This user is already an moderator.')
-    } else if (user.Role.role === 'System') {
-      throw errors.parameterError('role', 'This user cannot be changed.')
+    if (user.Role.role === "Moderator") {
+      throw errors.parameterError("role", "This user is already an moderator.");
+    } else if (user.Role.role === "System") {
+      throw errors.parameterError("role", "This user cannot be changed.");
     }
 
     // promote the user to admin
-    await Role.setUserRole(user)
-    await Role.setRole('Moderator')
+    await Role.setUserRole(user);
+    await Role.setRole("Moderator");
 
     const roleReload = Role.reload({
       include: [
-        { model: Role, attributes: ['role'] },
-        { model: User, attributes: ['id', 'username', 'color', 'avatar'] }
+        { model: Role, attributes: ["role"] },
+        { model: User, attributes: ["id", "username", "color", "avatar"] }
       ]
-    })
+    });
 
     return roleReload;
-  }
+  };
 
   // create the system user
-  Role.prototype.createSystem = function (id) {
-    const { Role, User } = sequelize.models
+  Role.prototype.createSystem = async function(id) {
+    const { Role, User } = sequelize.models;
 
-    const user = await User.findById(id)
+    const user = await User.findById(id);
 
     if (!user) {
       throw errors.sequelizeValidation(sequelize, {
-        errors: 'User does not exist.',
+        errors: "User does not exist.",
         userId: id
-      })
+      });
     }
 
     // promote the user to admin
-    await Role.setUserRole(user)
-    await Role.setRole('System')
+    await Role.setUserRole(user);
+    await Role.setRole("System");
 
     const roleReload = Role.reload({
       include: [
-        { model: Role, attributes: ['role'] },
-        { model: User, attributes: ['id', 'username', 'color', 'avatar'] }
+        { model: Role, attributes: ["role"] },
+        { model: User, attributes: ["id", "username", "color", "avatar"] }
       ]
-    })
+    });
 
     return roleReload;
-  }
-
+  };
 
   return Role;
 };
