401992a923d5a04955817e41f4c749e2fff8186e
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-01 00:39:41 +1000

    fixed a typo

--- models/ipaddress.js
+++ models/ipaddress.js
@@ -17,21 +17,19 @@ module.exports = (sequelize, DataTypes) => {
   // class methods
 
   IpAddress.associate = function(models) {
-    IpAddress.belongsToMany(models.User, { throught: "UserIp" });
+    IpAddress.belongsToMany(models.User, { through: "UserIp" });
   };
 
   IpAddress.createIpIfEmpty = async function(ip, user) {
     const { User } = sequelize.models;
     // get ip if user had one already
-    const ip = await IpAddress.findOne({
+    const ipa = await IpAddress.findOne({
       where: { ipaddress: ip },
       include: [{ model: User, where: { id: user.id } }]
     });
 
-    if (!ip) {
-      const newIp = await IpAddress.create({
-        ipaddress: ip
-      });
+    if (!ipa) {
+      const newIp = await IpAddress.create({ ipaddress: ip });
       await newIp.addUser(user);
     }
   };
