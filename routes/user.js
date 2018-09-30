af95385e0f90f1bfb8ffff98d766b20a519d0bd7
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-01 00:56:26 +1000

    updated user controller

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const controller = require("../controllers/user/root");
+
+router.get("/", controller.root);
+
 module.exports = router;
