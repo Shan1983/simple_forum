f034bb64034325498ccd26c5d6672198d3241d95
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-01 23:54:04 +1000

    updated user index controller - with get all users

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,53 @@
-exports.index = (req, res, next) => {
+const multer = require("multer");
+const bcrypt = require("bcryptjs");
+const sharp = require("sharp");
+const session = require("../controllers/userSession/userSession");
+const {
+  User,
+  Post,
+  Thread,
+  Category,
+  Sequelize,
+  IpAdress,
+  Ban
+} = require("../models");
+const pagination = require("../helpers/pagination");
+const errors = require("../helpers/mainErrors");
+const jwtHelper = require("../helpers/jwtHelper");
+
+/*
+sharp(profilePicture.file)
+            .resize(400, 400)
+            .max()
+            .toBuffer((err, buff) => {
+              profilePicture.file = buff;
+              cb(err || null, options);
+            });
+*/
+
+/**
+ * Returns a list of all users, this route is only for
+ * Administrators
+ * @param {*} req
+ */
+exports.index = async (req, res, next) => {
   try {
-    res.json({ status: "OK" });
+    const token = req.headers.authorization;
+    if (jwtHelper.getUserRole(req, token) === req.session.admin) {
+      const users = await User.findAll({
+        attributes: { exclude: ["password"] }
+      });
+
+      if (!users) {
+        throw errors.noUsers;
+      }
+
+      // send the users
+      res.json({ users });
+    } else {
+      res.json({});
+    }
   } catch (error) {
     next(error);
   }
-};
-
-exports.login = (req, res, next) => {
-  try {
-    res.json({ secret: process.env.JWT_SECRET });
-  } catch (error) {}
 };
