d20e8edc1ba642e1dfa40a04f4b311d648fef745
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-02 01:34:31 +1000

    added more routes

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,26 @@ const controller = require("../controllers");
 
 router.get("/", passport.authenticate("jwt"), controller.getAllUsers);
 router.get("/:username", passport.authenticate("jwt"), controller.getUserMeta);
+router.get(
+  "/profile/:username",
+  passport.authenticate("jwt"),
+  controller.userProfile
+);
 
-// router.get("/login", passport.authenticate("jwt"), controller.login);
+router.post("/login", controller.login);
+router.post("/register", controller.register);
+router.post("/logout", controller.logout);
+
+router.put(
+  "/profile/:username",
+  passport.authenticate("jwt"),
+  controller.updateProfile
+);
+
+router.delete(
+  "/profile/:username",
+  passport.authenticate("jwt"),
+  controller.updateProfile
+);
 
 module.exports = router;
