6dadd804434c727fa0b47edb162c659032066505
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-01 23:54:48 +1000

    fixed an error with jwthelper - next caused some funcky sequelize stuff

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -2,6 +2,8 @@ const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const moment = require("moment");
 
+const errors = require("./mainErrors");
+
 module.exports = {
   getJwtSecret() {
     return process.env.JWT_SECRET;
@@ -20,5 +22,18 @@ module.exports = {
       exp: tomorrow,
       iat: now
     });
+  },
+  getUserRole(req, token) {
+    if (req.session.admin) {
+      const decoded = jwt.verify(token, this.getJwtSecret);
+      if (!decoded) {
+        throw errors.parameterError("token", "We could not decode the token.");
+      } else {
+        console.log("passed the token");
+        return decoded.sub.role;
+      }
+    } else {
+      throw errors.notAuthorized;
+    }
   }
 };
