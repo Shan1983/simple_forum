b620c577c905d9a3e765516609f65bb299271efd
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-04 02:02:32 +1000

    changed migration to suit associations better

--- migrations/20180923105448-create-user.js
+++ migrations/20180923105448-create-user.js
@@ -10,67 +10,29 @@ module.exports = {
           primaryKey: true,
           type: Sequelize.INTEGER
         },
-        username: {
+        username: { type: Sequelize.STRING },
+        description: { type: Sequelize.TEXT },
+        colorIcon: { type: Sequelize.STRING },
+        password: { type: Sequelize.STRING },
+        email: { type: Sequelize.STRING, unique: true },
+        RoleId: { type: Sequelize.INTEGER },
+        VillageId: { type: Sequelize.INTEGER },
+        points: { type: Sequelize.INTEGER, defautValue: 0 },
+        emailVerificationToken: {
           type: Sequelize.STRING
         },
-        description: {
-          type: Sequelize.TEXT
-        },
-        colorIcon: {
-          type: Sequelize.STRING
-        },
-        password: {
-          type: Sequelize.STRING
-        },
-        email: {
-          type: Sequelize.STRING,
-          unique: true
-        },
-        RoleId: {
-          type: Sequelize.INTEGER
-        },
-        VillageId: {
-          type: Sequelize.INTEGER
-        },
-        points: {
-          type: Sequelize.INTEGER,
-          defautValue: 0
-        },
-        facebookToken: {
-          type: Sequelize.STRING
-        },
-        googleToken: {
-          type: Sequelize.STRING
-        },
-        FriendId: {
-          type: Sequelize.INTEGER
-        },
-        avatar: {
-          type: Sequelize.STRING
-        },
-        IpId: {
-          type: Sequelize.INTEGER
-        },
-        allowAdvertising: {
+        emailVerified: {
           type: Sequelize.BOOLEAN,
-          defautValue: false
+          defaultValue: false
         },
-        emailSubscriptions: {
-          type: Sequelize.BOOLEAN,
-          defaultValue: true
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE
-        }
+        FriendId: { type: Sequelize.INTEGER },
+        avatar: { type: Sequelize.STRING },
+        allowAdvertising: { type: Sequelize.BOOLEAN, defautValue: false },
+        emailSubscriptions: { type: Sequelize.BOOLEAN, defaultValue: true },
+        createdAt: { allowNull: false, type: Sequelize.DATE },
+        updatedAt: { allowNull: false, type: Sequelize.DATE }
       },
-      {
-        charset: "utf8mb4"
-      }
+      { charset: "utf8mb4" }
     );
   },
   down: (queryInterface, Sequelize) => {
