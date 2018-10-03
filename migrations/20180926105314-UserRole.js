c190bd2bed27096b40ded989d056d15fc69f9831
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-04 02:09:01 +1000

    added cross ref table for user roles

---
+++ migrations/20180926105314-UserRole.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("userroles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      UserRoleId: {
+        type: Sequelize.INTEGER
+      },
+      RoleId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("userroles");
+  }
+};
