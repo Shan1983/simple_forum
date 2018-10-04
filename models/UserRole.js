868088a5bd2bbed297d88cc3fa37e5231683b13e
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-04 19:55:43 +1000

    added user role model to help assign a user a role

---
+++ models/UserRole.js
@@ -0,0 +1,25 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const UserRole = sequelize.define(
+    "UserRole",
+    {
+      UserId: DataTypes.INTEGER,
+      RoleId: DataTypes.INTEGER
+    },
+    {}
+  );
+  UserRole.associate = function(models) {
+    UserRole.belongsTo(models.User, { through: "userroles" });
+    UserRole.belongsTo(models.Role, { through: "userroles" });
+  };
+
+  UserRole.assignRole = async function(user) {
+    console.log("id", user.id);
+    await UserRole.create({
+      UserId: user.id,
+      RoleId: 1
+    });
+  };
+
+  return UserRole;
+};
