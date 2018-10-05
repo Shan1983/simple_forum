9fb2a16e9d111fdb89ce3c64f3ec2bd22788a125
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-06 00:39:36 +1000

    updated seed user file

--- seeders/20181004151644-originalAdmin.js
+++ seeders/20181004151644-originalAdmin.js
@@ -1,6 +1,8 @@
 "use strict";
 const bcrypt = require("bcryptjs");
 
+const password = bcrypt.hashSync("secret", 10);
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
@@ -10,7 +12,7 @@ module.exports = {
           username: "Shan",
           email: "shan@test.com",
           colorIcon: "#000000",
-          password: bcrypt.hashSync("secret", 10),
+          password,
           emailVerificationToken: "123456789",
           emailVerified: true,
           RoleId: 3,
