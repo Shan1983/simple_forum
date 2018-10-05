05e3aceb2b6b69fb2366d935a8d834ad3afc8e08
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-05 15:11:28 +1000

    user route testing

--- test/user.test.js
+++ test/user.test.js
@@ -3,33 +3,46 @@ process.env.NODE_ENV = "test";
 const server = require("../server");
 const chai = require("chai");
 const should = chai.should();
-const bcrypt = require("bcryptjs");
-const uuidv5 = require("uuid/v5");
 const errors = require("../helpers/mainErrors");
-const faker = require("faker");
+const jwtHelper = require("../helpers/jwtHelper");
+const { User } = require("../models");
 
 chai.use(require("chai-http"));
 chai.use(require("chai-things"));
 
-describe("User Routes", () => {
+const bcrypt = require("bcryptjs");
+const faker = require("faker");
+
+const username = faker.internet.userName();
+const email = faker.internet.email();
+const password = bcrypt.hashSync("secret", 10);
+
+const login = async (email, password) =>
+  await chai
+    .request(server)
+    .post("/api/v1/user/login")
+    .set("content-type", "application/json")
+    .send({
+      email,
+      password
+    });
+
+describe("USER", () => {
   // lets get this party started!
 
-  before(async done => {
-    if (server.locals.started) {
-      done();
-    }
+  // before(done => {
+  //   if (server.locals.started) {
+  //     done();
+  //   }
 
-    // server.on("started", () => {
-    //   done();
-    // });
-  });
+  //   // server.on("started", () => {
+  //   //   done();
+  //   // });
+  // });
 
-  describe("/ POST - user", () => {
-    const username = faker.internet.userName();
-    const email = faker.internet.email();
-    const password = bcrypt.hashSync("secret", 10);
-    const token = uuidv5(email, uuidv5.DNS);
+  describe("/POST - USER ROUTES", () => {
     //let agent = chai.request.agent(server);
+
     it("should register a new user", done => {
       chai
         .request(server)
@@ -39,8 +52,6 @@ describe("User Routes", () => {
           username,
           email,
           password,
-          emailVerificationToken: token,
-          emailVerified: false,
           RoleId: 1
         })
         .end((err, res) => {
@@ -52,12 +63,15 @@ describe("User Routes", () => {
         });
     });
 
-    it("should prevent a user from logging in", done => {
+    it("should fail while awaiting email verification", done => {
       chai
         .request(server)
         .post("/api/v1/user/login")
         .set("content-type", "application/json")
-        .send({ email, password })
+        .send({
+          email,
+          password
+        })
         .end((err, res) => {
           res.should.have.status(400);
           // console.log(res);
@@ -70,20 +84,15 @@ describe("User Routes", () => {
     });
 
     it('should assign the user a role of "member"', async () => {
+      const userToken = await User.findById(2);
+
       const tokenRes = await chai
         .request(server)
-        .get("/api/v1/user/verify/email/" + token);
+        .get("/api/v1/user/verify/email/" + userToken.emailVerificationToken);
 
       tokenRes.should.have.status(200);
 
-      const memberRes = await chai
-        .request(server)
-        .post("/api/v1/user/login")
-        .set("content-type", "application/json")
-        .send({
-          email,
-          password
-        });
+      const memberRes = await login(email, password);
 
       memberRes.should.have.status(200);
       memberRes.body.should.have.property("success", true);
@@ -91,20 +100,22 @@ describe("User Routes", () => {
     });
 
     it("should allow a user to login", async () => {
-      const res = await chai
-        .request(server)
-        .post("/api/v1/user/login")
-        .set("content-type", "application/json")
-        .send({
-          email,
-          password
-        });
+      const res = await login(email, password);
 
       res.should.have.status(200);
       res.body.should.have.property("success", true);
       res.body.should.have.property("username", username);
       res.body.should.have.property("role", "Member");
       res.body.should.have.property("token");
+    });
+  });
+
+  describe("/GET - USER ROUTES", () => {
+    it("should return a list of users - must be admin", done => {});
+    it("should NOT return a list of users", async () => {
+      // 1. log in
+      // 2. check the users role
+      // 3. return not authorized error
     });
   });
 });
