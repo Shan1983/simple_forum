15644b71135fca747a723eb34147d74f9c5ba0fc
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-07 04:49:01 +1100

    added the category routes

--- routes/category.js
+++ routes/category.js
@@ -4,4 +4,28 @@ const router = express.Router();
 
 const controller = require("../controllers/category/category");
 
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.getCategory
+);
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.newCategory
+);
+
+router.put(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.updateCategory
+);
+
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.deleteCategory
+);
+
 module.exports = router;
