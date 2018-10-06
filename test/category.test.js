6a7a89bb73fb7835c20e0cf6f3f0d78b1628f9d1
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-07 07:48:07 +1100

    completed category tests

--- test/category.test.js
+++ test/category.test.js
@@ -5,23 +5,24 @@ const chai = require("chai");
 const should = chai.should();
 const expect = chai.expect;
 const errors = require("../helpers/mainErrors");
-const jwtHelper = require("../helpers/jwtHelper");
-const { User, Category } = require("../models");
 
 chai.use(require("chai-http"));
 chai.use(require("chai-things"));
 
-const bcrypt = require("bcryptjs");
-const faker = require("faker");
-
-const username = faker.internet.userName();
-const email = faker.internet.email();
-const password = bcrypt.hashSync("secret", 10);
-
 describe("Category", () => {
-  describe("/GET - CATEGORY ROUTES", () => {
-    describe("GET /api/v1/category/", () => {
-      it("should NOT return all categories", async () => {
+  describe("/POST - CATEGORY ROUTES", () => {
+    describe("POST /api/v1/category/", () => {
+      it("should NOT post a new category", async () => {
+        const agent = chai.request.agent(server);
+
+        const category = await agent
+          .post(`/api/v1/category`)
+          .set("content-type", "application/json");
+
+        category.should.have.status(401);
+      });
+
+      it("should post a new category", async () => {
         const agent = chai.request.agent(server);
 
         const user = await agent.post("/api/v1/user/login").send({
@@ -35,14 +36,53 @@ describe("Category", () => {
         const cookie = `token=${user.body.token}`;
 
         const category = await agent
-          .get(`/api/v1/category`)
+          .post(`/api/v1/category`)
           .set("content-type", "application/json")
-          .set("Authorization", token);
+          .set("Authorization", token)
+          .set("Cookie", cookie)
+          .send({ title: "test", description: "test" });
+
+        category.should.have.status(200);
+        category.body.should.have.property("success", true);
+      });
+
+      it("should NOT post a duplicate category", async () => {
+        const agent = chai.request.agent(server);
+
+        const user = await agent.post("/api/v1/user/login").send({
+          email: "shan@test.com",
+          password: "secret"
+        });
+
+        user.should.have.status(200);
+
+        const token = `Bearer ${user.body.token}`;
+        const cookie = `token=${user.body.token}`;
+
+        const category = await agent
+          .post(`/api/v1/category`)
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .set("Cookie", cookie)
+          .send({ title: "test", description: "test" });
 
         category.should.have.status(400);
-        category.body.errors.should.include.something.that.deep.equals(
-          errors.categoryError
+        category.body.error.should.include.something.that.deep.equals(
+          errors.categoryTitleError
         );
+      });
+    });
+  });
+  describe("/GET - CATEGORY ROUTES", () => {
+    describe("GET /api/v1/category/", () => {
+      it("should NOT return all categories", async () => {
+        const agent = chai.request.agent(server);
+
+        const category = await agent
+          .get(`/api/v1/category`)
+          .set("content-type", "application/json");
+
+        category.should.have.status(401);
       });
 
       it("should return all categories", async () => {
@@ -67,5 +107,190 @@ describe("Category", () => {
         category.should.have.status(200);
       });
     });
+
+    describe("GET /api/v1/category/:id/threads", () => {
+      it("should NOT return categories and their threads", async () => {
+        const agent = chai.request.agent(server);
+
+        const category = await agent
+          .get(`/api/v1/category/1/threads`)
+          .set("content-type", "application/json");
+
+        category.should.have.status(401);
+      });
+      it("should return a category and its threads", async () => {
+        const agent = chai.request.agent(server);
+
+        const user = await agent.post("/api/v1/user/login").send({
+          email: "shan@test.com",
+          password: "secret"
+        });
+
+        user.should.have.status(200);
+
+        const token = `Bearer ${user.body.token}`;
+        const cookie = `token=${user.body.token}`;
+
+        const category = await agent
+          .get(`/api/v1/category/1/threads`)
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .set("Cookie", cookie);
+
+        category.should.have.status(200);
+      });
+    });
+  });
+
+  describe("/PUT - CATEGORY ROUTES", () => {
+    describe("PUT /api/v1/category/:id", () => {
+      it("should NOT update a category", async () => {
+        const agent = chai.request.agent(server);
+
+        const category = await agent
+          .put(`/api/v1/category/1`)
+          .set("content-type", "application/json")
+          .send({ title: "spiders", description: "scary" });
+
+        category.should.have.status(401);
+      });
+
+      it("should NOT update a category if user is not an ADMIN", async () => {
+        const agent = chai.request.agent(server);
+
+        const user = await agent.post("/api/v1/user/login").send({
+          email: "moderator@test.com",
+          password: "secret"
+        });
+
+        user.should.have.status(200);
+
+        const token = `Bearer ${user.body.token}`;
+        const cookie = `token=${user.body.token}`;
+
+        const category = await agent
+          .put(`/api/v1/category/1`)
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .set("Cookie", cookie)
+          .send({ title: "spiders", description: "scary" });
+
+        category.should.have.status(401);
+      });
+
+      it("should update a category", async () => {
+        const agent = chai.request.agent(server);
+
+        const user = await agent.post("/api/v1/user/login").send({
+          email: "shan@test.com",
+          password: "secret"
+        });
+
+        user.should.have.status(200);
+
+        const token = `Bearer ${user.body.token}`;
+        const cookie = `token=${user.body.token}`;
+
+        const category = await agent
+          .put(`/api/v1/category/1`)
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .set("Cookie", cookie)
+          .send({ title: "spiders", description: "scary" });
+
+        category.should.have.status(200);
+      });
+    });
+  });
+
+  describe("/DELETE - CATEGORY ROUTES", () => {
+    describe("DELETE /api/v1/category/:id", () => {
+      it("should NOT delete a category", async () => {
+        const agent = chai.request.agent(server);
+
+        const category = await agent
+          .delete(`/api/v1/category/1`)
+          .set("content-type", "application/json");
+
+        category.should.have.status(401);
+      });
+
+      it("should NOT delete a category if user is not an ADMIN", async () => {
+        const agent = chai.request.agent(server);
+
+        const user = await agent.post("/api/v1/user/login").send({
+          email: "moderator@test.com",
+          password: "secret"
+        });
+
+        user.should.have.status(200);
+
+        const token = `Bearer ${user.body.token}`;
+        const cookie = `token=${user.body.token}`;
+
+        const category = await agent
+          .delete(`/api/v1/category/1`)
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .set("Cookie", cookie);
+
+        category.should.have.status(401);
+      });
+
+      it("should update a category", async () => {
+        const agent = chai.request.agent(server);
+
+        const user = await agent.post("/api/v1/user/login").send({
+          email: "shan@test.com",
+          password: "secret"
+        });
+
+        user.should.have.status(200);
+
+        const token = `Bearer ${user.body.token}`;
+        const cookie = `token=${user.body.token}`;
+
+        const category = await agent
+          .delete(`/api/v1/category/1`)
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .set("Cookie", cookie);
+
+        category.should.have.status(200);
+        category.body.should.have.property("success", true);
+      });
+    });
+  });
+});
+
+describe("SETUP STEP", () => {
+  it("should post a new category", async () => {
+    const agent = chai.request.agent(server);
+
+    const user = await agent.post("/api/v1/user/login").send({
+      email: "shan@test.com",
+      password: "secret"
+    });
+
+    user.should.have.status(200);
+
+    const token = `Bearer ${user.body.token}`;
+    const cookie = `token=${user.body.token}`;
+
+    const category = await agent
+      .post(`/api/v1/category`)
+      .set("content-type", "application/json")
+      .set("Authorization", token)
+      .set("Cookie", cookie)
+      .send({ title: "javascript", description: "test" });
+    const category2 = await agent
+      .post(`/api/v1/category`)
+      .set("content-type", "application/json")
+      .set("Authorization", token)
+      .set("Cookie", cookie)
+      .send({ title: "npm", description: "test" });
+
+    category.should.have.status(200);
+    category2.should.have.status(200);
   });
 });
