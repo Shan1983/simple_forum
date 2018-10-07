3a3cd7e820b926b0c69ad34b8539bb27710e6c3c
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-08 05:19:24 +1100

    added post action for threads

--- controllers/thread/index.js
+++ controllers/thread/index.js
@@ -1,7 +1,36 @@
 const { User, Category, Thread } = require("../../models");
+const errors = require("../../helpers/mainErrors");
+const slug = require("slugify");
 
+// /:category/
 exports.postNewThread = async (req, res, next) => {
   try {
+    if (req.session.userId) {
+      const param = req.params.category;
+
+      const category = await Category.findOne({
+        where: { title: param }
+      });
+
+      const categoryAttributes = category.getAttributes(category);
+
+      console.log(req.body.discussion);
+
+      if (!category) {
+        res.status(400);
+        res.json({ error: [errors.categoryError] });
+      } else {
+        const thread = await Thread.create({
+          title: req.body.title,
+          slug: slug(req.body.slug),
+          CategoryId: categoryAttributes.id,
+          UserId: req.session.userId,
+          discussion: req.body.discussion
+        });
+
+        res.json(thread.toJSON());
+      }
+    }
   } catch (error) {
     next(error);
   }
