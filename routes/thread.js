f325c33235c1174babd7cdd58561a11fa8d48fd4
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-08 05:21:34 +1100

    changed route

--- routes/thread.js
+++ routes/thread.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const controller = require("../controllers/thread");
 
 router.post(
-  "/",
+  "/:category/",
   passport.authenticate("jwt", { session: false }),
   controller.postNewThread
 );
