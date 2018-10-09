da664d596ecfbf4e0ad6ae136e0a6b429d0a85ea
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-10 01:25:19 +1100

    completed post test

--- test/cases/post.test.js
+++ test/cases/post.test.js
@@ -161,6 +161,141 @@ describe("POST", () => {
       });
     });
   });
-  describe("/PUT POST ROUTES", () => {});
-  describe("/DELETE POST ROUTES", () => {});
+  describe("/PUT POST ROUTES", () => {
+    describe("PUT /:postId", () => {
+      it("should not proceed if not authenitcated", async () => {
+        const agent = chai.request.agent(server);
+        const thread = await agent
+          .put(`/api/v1/post/1`)
+          .set("content-type", "application/json");
+
+        thread.should.have.status(401);
+      });
+      it("should not proceed if post is NOT found", async () => {
+        const agent = chai.request.agent(server);
+        const login = await agent.post("/api/v1/user/login").send({
+          email: "fred@test.com",
+          password: "secret"
+        });
+
+        login.should.have.status(200);
+
+        const token = `Bearer ${login.body.token}`;
+
+        const post = await agent
+          .put(`/api/v1/post/99`)
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({ discussion: "test" });
+
+        post.should.have.status(400);
+      });
+      it("should not update if user is NOT OP", async () => {
+        const agent = chai.request.agent(server);
+        const login = await agent.post("/api/v1/user/login").send({
+          email: "turtle@test.com",
+          password: "test123"
+        });
+
+        login.should.have.status(200);
+
+        const token = `Bearer ${login.body.token}`;
+
+        const post = await agent
+          .put(`/api/v1/post/1`)
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({ discussion: "test" });
+
+        post.should.have.status(401);
+      });
+      it("should update a post", async () => {
+        const agent = chai.request.agent(server);
+        const login = await agent.post("/api/v1/user/login").send({
+          email: "fred@test.com",
+          password: "secret"
+        });
+
+        login.should.have.status(200);
+
+        const token = `Bearer ${login.body.token}`;
+
+        const post = await agent
+          .put(`/api/v1/post/1`)
+          .set("content-type", "application/json")
+          .set("Authorization", token)
+          .send({ discussion: "updated!" });
+
+        post.should.have.status(200);
+        post.body.should.have.property("success");
+      });
+    });
+  });
+  describe("/DELETE POST ROUTES", () => {
+    describe("DELETE /:postId", () => {
+      it("should not proceed if not authenitcated", async () => {
+        const agent = chai.request.agent(server);
+        const thread = await agent
+          .delete(`/api/v1/post/1`)
+          .set("content-type", "application/json");
+
+        thread.should.have.status(401);
+      });
+      it("should not proceed if post is NOT found", async () => {
+        const agent = chai.request.agent(server);
+        const login = await agent.post("/api/v1/user/login").send({
+          email: "fred@test.com",
+          password: "secret"
+        });
+
+        login.should.have.status(200);
+
+        const token = `Bearer ${login.body.token}`;
+
+        const post = await agent
+          .delete(`/api/v1/post/99`)
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        post.should.have.status(400);
+      });
+      it("should not update if user is NOT an ADMIN", async () => {
+        const agent = chai.request.agent(server);
+        const login = await agent.post("/api/v1/user/login").send({
+          email: "fred@test.com",
+          password: "secret"
+        });
+
+        login.should.have.status(200);
+
+        const token = `Bearer ${login.body.token}`;
+
+        const post = await agent
+          .delete(`/api/v1/post/1`)
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        post.should.have.status(401);
+      });
+      it("should delete a post", async () => {
+        const agent = chai.request.agent(server);
+        const login = await agent.post("/api/v1/user/login").send({
+          email: "turtle@test.com",
+          password: "test123"
+        });
+
+        login.should.have.status(200);
+
+        const token = `Bearer ${login.body.token}`;
+
+        const post = await agent
+          .delete(`/api/v1/post/1`)
+          .set("content-type", "application/json")
+          .set("Authorization", token);
+
+        post.should.have.status(200);
+        post.body.should.have.property("success");
+      });
+    });
+  });
 });
