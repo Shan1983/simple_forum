b00f32272dd532751e5a35bfd8d35415b2f0dd02
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-10 17:29:00 +1100

    updated post controller actions to make them more secure

--- controllers/post/index.js
+++ controllers/post/index.js
@@ -8,41 +8,53 @@ const {
   Rewards
 } = require("../../models");
 const errors = require("../../helpers/mainErrors");
+const jwtHelper = require("../../helpers/jwtHelper");
 
 // "/:threadId",
 exports.newPost = async (req, res, next) => {
   try {
-    const thread = await Thread.findById(req.params.threadId);
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      const thread = await Thread.findById(req.params.threadId);
 
-    const attr = thread.getAttributes(thread);
+      const attr = thread.getAttributes(thread);
 
-    if (!thread) {
-      res.status(400);
-      res.json({ error: [errors.threadError] });
-    } else if (attr.locked) {
-      res.status(400);
-      res.json({ error: [errors.lockedError] });
+      if (!thread) {
+        res.status(400);
+        res.json({ error: [errors.threadError] });
+      } else if (attr.locked) {
+        res.status(400);
+        res.json({ error: [errors.lockedError] });
+      } else {
+        const discussion = req.body.discussion;
+
+        await Post.create({
+          discussion,
+          UserId: req.session.userId,
+          ThreadId: attr.id
+        });
+
+        const user = await User.findById(req.session.userId);
+        //   const rewards = await Rewards.findAll({
+        //     attributes: ["pointsPerPost"]
+        //   });
+
+        //   // fix this when u work on settings
+        //   const points = rewards.toJSON();
+
+        await user.increment("postCount", { by: 1 });
+        //   await rewards.increment("points", { by: points.pointsPerPost });
+
+        res.json({ success: true });
+      }
     } else {
-      const discussion = req.body.discussion;
-
-      await Post.create({
-        discussion,
-        UserId: req.session.userId,
-        ThreadId: attr.id
-      });
-
-      const user = await User.findById(req.session.userId);
-      //   const rewards = await Rewards.findAll({
-      //     attributes: ["pointsPerPost"]
-      //   });
-
-      //   // fix this when u work on settings
-      //   const points = rewards.toJSON();
-
-      await user.increment("postCount", { by: 1 });
-      //   await rewards.increment("points", { by: points.pointsPerPost });
-
-      res.json({ success: true });
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -52,22 +64,33 @@ exports.newPost = async (req, res, next) => {
 // can only be done by the OP
 exports.markAsBest = async (req, res, next) => {
   try {
-    const post = await Post.findById(req.params.postId);
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      const post = await Post.findById(req.params.postId);
 
-    if (!post) {
-      res.status(400);
-      res.json({ error: [errors.postError] });
-    } else {
-      const attributes = post.getAttributes(post);
-
-      if (req.session.userId !== attributes.UserId) {
-        res.status(401);
-        res.json({ error: [errors.notAuthorized] });
+      if (!post) {
+        res.status(400);
+        res.json({ error: [errors.postError] });
       } else {
-        await Post.markAsBest(attributes.id);
+        const attributes = post.getAttributes(post);
 
-        res.json({ success: true });
+        if (req.session.userId !== attributes.UserId) {
+          res.status(401);
+          res.json({ error: [errors.notAuthorized] });
+        } else {
+          await Post.markAsBest(attributes.id);
+
+          res.json({ success: true });
+        }
       }
+    } else {
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -77,36 +100,47 @@ exports.markAsBest = async (req, res, next) => {
 // /:threadId/:postId/quote
 exports.quote = async (req, res, next) => {
   try {
-    const post = await Post.findById(req.params.postId);
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      const post = await Post.findById(req.params.postId);
 
-    if (!post) {
-      res.status(400);
-      res.json({ error: [errors.postError] });
-    } else {
-      // get the post to be quoted
-      const quote = post.getAttributes(post);
-
-      // get the thread and check if it exists
-      const thread = await Thread.findById(req.params.threadId);
-
-      if (!thread) {
+      if (!post) {
         res.status(400);
-        res.json({ error: [errors.threadError] });
+        res.json({ error: [errors.postError] });
       } else {
-        const threadAttributes = thread.getAttributes(thread);
-        const { discussion } = req.body;
+        // get the post to be quoted
+        const quote = post.getAttributes(post);
 
-        // create a post with the added quote
-        await Post.create({
-          UserId: req.session.userId,
-          ThreadId: threadAttributes.id,
-          discussion: `<blockquote>${threadAttributes.discussion}</blockquote>
+        // get the thread and check if it exists
+        const thread = await Thread.findById(req.params.threadId);
+
+        if (!thread) {
+          res.status(400);
+          res.json({ error: [errors.threadError] });
+        } else {
+          const threadAttributes = thread.getAttributes(thread);
+          const { discussion } = req.body;
+
+          // create a post with the added quote
+          await Post.create({
+            UserId: req.session.userId,
+            ThreadId: threadAttributes.id,
+            discussion: `<blockquote>${threadAttributes.discussion}</blockquote>
           <hr/>
           ${discussion}`
-        });
+          });
 
-        res.json({ success: true });
+          res.json({ success: true });
+        }
       }
+    } else {
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -116,22 +150,33 @@ exports.quote = async (req, res, next) => {
 // must be OP
 exports.updatePost = async (req, res, next) => {
   try {
-    const post = await Post.findById(req.params.postId);
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      const post = await Post.findById(req.params.postId);
 
-    if (!post) {
-      res.status(400);
-      res.json({ error: [errors.postError] });
-    } else {
-      const postAttr = post.getAttributes(post);
-      if (req.session.userId === postAttr.UserId) {
-        post.update({
-          discussion: req.body.discussion
-        });
-        res.json({ success: true });
+      if (!post) {
+        res.status(400);
+        res.json({ error: [errors.postError] });
       } else {
-        res.status(401);
-        res.json({ error: [errors.notAuthorized] });
+        const postAttr = post.getAttributes(post);
+        if (req.session.userId === postAttr.UserId) {
+          post.update({
+            discussion: req.body.discussion
+          });
+          res.json({ success: true });
+        } else {
+          res.status(401);
+          res.json({ error: [errors.notAuthorized] });
+        }
       }
+    } else {
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -141,20 +186,33 @@ exports.updatePost = async (req, res, next) => {
 // must be admin
 exports.deletePost = async (req, res, next) => {
   try {
-    const post = await Post.findById(req.params.postId);
+    const token = req.cookies.token;
 
-    if (!post) {
-      res.status(400);
-      res.json({ error: [errors.postError] });
-    } else {
-      if (req.session.role !== "Admin") {
-        res.status(401);
-        res.json({ error: [errors.notAuthorized] });
+    const decodedToken = jwtHelper.decodeJwt(token);
+
+    if (decodedToken.id === req.session.userId) {
+      const post = await Post.findById(req.params.postId);
+
+      if (!post) {
+        res.status(400);
+        res.json({ error: [errors.postError] });
       } else {
-        await post.destroy();
+        if (req.session.role !== "Admin") {
+          res.status(401);
+          res.json({ error: [errors.notAuthorized] });
+        } else {
+          await post.destroy();
 
-        res.json({ success: true });
+          res.json({ success: true });
+        }
       }
+    } else {
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
