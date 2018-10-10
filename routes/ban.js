92fdb4e6e1f61780fc636d42e2beb8af5d89e5ad
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-11 06:09:42 +1100

    added ban routes

--- routes/ban.js
+++ routes/ban.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
+passport = require("passport");
 
 const controller = require("../controllers/ban/");
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.addBan
+);
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.removeBan
+);
 
 module.exports = router;
