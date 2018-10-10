72ba5fdd56f9b09795d1b3c68736e70eaff117ae
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-10 17:29:26 +1100

    updated thread controller actions to make them more secure

--- controllers/thread/index.js
+++ controllers/thread/index.js
@@ -1,41 +1,54 @@
 const { User, Category, Thread, Post, Village } = require("../../models");
 const errors = require("../../helpers/mainErrors");
 const slug = require("slugify");
+const jwtHelper = require("../../helpers/jwtHelper");
 
 // /:category/
 exports.postNewThread = async (req, res, next) => {
   try {
-    if (req.session.userId) {
-      const param = req.params.category;
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
 
-      const category = await Category.findOne({
-        where: { id: param }
-      });
+    if (decodedToken.id === req.session.userId) {
+      if (req.session.userId) {
+        const param = req.params.category;
 
-      if (!category) {
-        res.status(400);
-        res.json({ error: [errors.categoryError] });
+        const category = await Category.findOne({
+          where: { id: param }
+        });
+
+        if (!category) {
+          res.status(400);
+          res.json({ error: [errors.categoryError] });
+        } else {
+          const categoryAttributes = category.getAttributes(category);
+          await Thread.create({
+            title: req.body.title,
+            slug: slug(req.body.slug),
+            CategoryId: categoryAttributes.id,
+            UserId: req.session.userId,
+            discussion: req.body.discussion
+          });
+
+          const user = await User.findById(req.session.userId);
+
+          await user.increment("postCount", { by: 1 });
+
+          res.json({
+            success: true
+          });
+        }
       } else {
-        const categoryAttributes = category.getAttributes(category);
-        await Thread.create({
-          title: req.body.title,
-          slug: slug(req.body.slug),
-          CategoryId: categoryAttributes.id,
-          UserId: req.session.userId,
-          discussion: req.body.discussion
-        });
-
-        const user = await User.findById(req.session.userId);
-
-        await user.increment("postCount", { by: 1 });
-
-        res.json({
-          success: true
-        });
+        res.status(401);
+        res.json({ error: [errors.notAuthorized] });
       }
     } else {
-      res.status(401);
-      res.json({ error: [errors.notAuthorized] });
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -45,19 +58,30 @@ exports.postNewThread = async (req, res, next) => {
 // /:threadId/lock
 exports.lockThread = async (req, res, next) => {
   try {
-    if (!req.session.role || req.session.role !== "Member") {
-      const thread = await Thread.findById(req.params.threadId);
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      if (!req.session.role || req.session.role !== "Member") {
+        const thread = await Thread.findById(req.params.threadId);
 
-      if (!thread) {
-        res.status(400);
-        res.json({ error: [errors.threadError] });
+        if (!thread) {
+          res.status(400);
+          res.json({ error: [errors.threadError] });
+        } else {
+          thread.lockThread(thread, req.body.reason, req.body.message);
+          res.json({ success: true });
+        }
       } else {
-        thread.lockThread(thread, req.body.reason, req.body.message);
-        res.json({ success: true });
+        res.status(401);
+        res.json({ error: [errors.notAuthorized] });
       }
     } else {
-      res.status(401);
-      res.json({ error: [errors.notAuthorized] });
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -67,25 +91,36 @@ exports.lockThread = async (req, res, next) => {
 // /:threadId/make-sticky
 exports.stickyThread = async (req, res, next) => {
   try {
-    if (!req.session.role || req.session.role !== "Member") {
-      const thread = await Thread.findById(req.params.threadId);
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      if (!req.session.role || req.session.role !== "Member") {
+        const thread = await Thread.findById(req.params.threadId);
 
-      if (!thread) {
-        res.status(400);
-        res.json({ error: [errors.threadError] });
-      } else {
-        const attr = thread.getAttributes(thread);
-        if (attr.locked) {
+        if (!thread) {
           res.status(400);
-          res.json({ error: [errors.lockedError] });
+          res.json({ error: [errors.threadError] });
         } else {
-          await thread.markAsSticky(thread, req.body.duration);
-          res.json({ success: true });
+          const attr = thread.getAttributes(thread);
+          if (attr.locked) {
+            res.status(400);
+            res.json({ error: [errors.lockedError] });
+          } else {
+            await thread.markAsSticky(thread, req.body.duration);
+            res.json({ success: true });
+          }
         }
+      } else {
+        res.status(401);
+        res.json({ error: [errors.notAuthorized] });
       }
     } else {
-      res.status(401);
-      res.json({ error: [errors.notAuthorized] });
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -95,37 +130,52 @@ exports.stickyThread = async (req, res, next) => {
 // /:threadId/move
 exports.moveThread = async (req, res, next) => {
   try {
-    if (!req.session.role || req.session.role !== "Member") {
-      const thread = await Thread.findById(req.params.threadId);
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      if (!req.session.role || req.session.role !== "Member") {
+        const thread = await Thread.findById(req.params.threadId);
 
-      if (!thread) {
-        res.status(400);
-        res.json({ error: [errors.threadError] });
-      } else {
-        const newCategory = await Category.findById(req.body.category);
-
-        if (!newCategory) {
+        if (!thread) {
           res.status(400);
-          res.json({ error: [errors.categoryError] });
+          res.json({ error: [errors.threadError] });
         } else {
-          const category = newCategory.getAttributes(newCategory);
-          const attr = thread.getAttributes(thread);
+          const newCategory = await Category.findById(req.body.category);
 
-          // move the thread to the new category
-          await thread.lockThread(thread, null, `Moved to: ${category.title}`);
+          if (!newCategory) {
+            res.status(400);
+            res.json({ error: [errors.categoryError] });
+          } else {
+            const category = newCategory.getAttributes(newCategory);
+            const attr = thread.getAttributes(thread);
 
-          if (attr.isSticky) {
-            await thread.removeSticky(thread);
+            // move the thread to the new category
+            await thread.lockThread(
+              thread,
+              null,
+              `Moved to: ${category.title}`
+            );
+
+            if (attr.isSticky) {
+              await thread.removeSticky(thread);
+            }
+
+            await thread.moveThread(attr, category);
+
+            res.json({ success: true });
           }
-
-          await thread.moveThread(attr, category);
-
-          res.json({ success: true });
         }
+      } else {
+        res.status(401);
+        res.json({ error: [errors.notAuthorized] });
       }
     } else {
-      res.status(401);
-      res.json({ error: [errors.notAuthorized] });
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -135,62 +185,73 @@ exports.moveThread = async (req, res, next) => {
 // /:threadId
 exports.getThread = async (req, res, next) => {
   try {
-    const thread = await Thread.findOne({
-      where: { id: req.params.threadId },
-      include: [
-        { model: Category, attributes: ["title"] },
-        {
-          model: Post,
-          attributes: ["discussion", "bestPost", "createdAt"],
-          include: [
-            {
-              model: User,
-              attributes: ["username", "colorIcon", "points", "postCount"],
-              includes: [
-                {
-                  model: Village,
-                  attributes: ["townhallLevel", "playerTag", "clanRole"]
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    });
-
-    if (!thread) {
-      res.status(400);
-      res.json({ error: [errors.threadError] });
-    } else {
-      const attr = thread.getAttributes(thread);
-
-      if (attr.locked) {
-        res.status(400);
-        res.json({ error: [errors.lockedError] });
-      } else {
-        // check sticky duration
-        if (attr.isSticky) {
-          if (attr.duration > new Date()) {
-            thread.removeSticky(thread);
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      const thread = await Thread.findOne({
+        where: { id: req.params.threadId },
+        include: [
+          { model: Category, attributes: ["title"] },
+          {
+            model: Post,
+            attributes: ["discussion", "bestPost", "createdAt"],
+            include: [
+              {
+                model: User,
+                attributes: ["username", "colorIcon", "points", "postCount"],
+                includes: [
+                  {
+                    model: Village,
+                    attributes: ["townhallLevel", "playerTag", "clanRole"]
+                  }
+                ]
+              }
+            ]
           }
-        }
+        ]
+      });
 
-        res.json({
-          title: attr.title,
-          slug: attr.slug,
-          postCount: attr.postCount,
-          locked: attr.locked,
-          lockedReason: attr.lockedReason,
-          lockedMessage: attr.lockedMessage,
-          isSticky: attr.isSticky,
-          stickyDuration: attr.stickyDuration,
-          titleBGColor: attr.titleBGColor,
-          discussion: attr.discussion,
-          createdAt: attr.createdAt,
-          Category: attr.Category.title,
-          Posts: attr.Posts
-        });
+      if (!thread) {
+        res.status(400);
+        res.json({ error: [errors.threadError] });
+      } else {
+        const attr = thread.getAttributes(thread);
+
+        if (attr.locked) {
+          res.status(400);
+          res.json({ error: [errors.lockedError] });
+        } else {
+          // check sticky duration
+          if (attr.isSticky) {
+            if (attr.duration > new Date()) {
+              thread.removeSticky(thread);
+            }
+          }
+
+          res.json({
+            title: attr.title,
+            slug: attr.slug,
+            postCount: attr.postCount,
+            locked: attr.locked,
+            lockedReason: attr.lockedReason,
+            lockedMessage: attr.lockedMessage,
+            isSticky: attr.isSticky,
+            stickyDuration: attr.stickyDuration,
+            titleBGColor: attr.titleBGColor,
+            discussion: attr.discussion,
+            createdAt: attr.createdAt,
+            Category: attr.Category.title,
+            Posts: attr.Posts
+          });
+        }
       }
+    } else {
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -200,38 +261,49 @@ exports.getThread = async (req, res, next) => {
 // /:categoryId/deleted/threads
 exports.getDeletedThreads = async (req, res, next) => {
   try {
-    if (req.session.role !== "Member") {
-      const category = await Category.findOne({
-        where: { id: req.params.categoryId },
-        include: [{ model: Thread, paranoid: false }]
-      });
-
-      if (!category) {
-        res.status(400);
-        res.json({ error: [errors.categoryError] });
-      } else {
-        const deletedItems = category.getAttributes(category);
-
-        const deleted = deletedItems.Threads.map(e => {
-          if (e.deletedAt) {
-            return {
-              category: deletedItems.title,
-              title: e.title,
-              slug: e.slug,
-              postCount: e.postCount,
-              titleBGColor: e.titleBGColor,
-              discussion: e.discussion,
-              deletedAt: e.deletedAt,
-              createdAt: e.createdAt
-            };
-          }
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      if (req.session.role !== "Member") {
+        const category = await Category.findOne({
+          where: { id: req.params.categoryId },
+          include: [{ model: Thread, paranoid: false }]
         });
 
-        res.json({ deletedThreads: deleted });
+        if (!category) {
+          res.status(400);
+          res.json({ error: [errors.categoryError] });
+        } else {
+          const deletedItems = category.getAttributes(category);
+
+          const deleted = deletedItems.Threads.map(e => {
+            if (e.deletedAt) {
+              return {
+                category: deletedItems.title,
+                title: e.title,
+                slug: e.slug,
+                postCount: e.postCount,
+                titleBGColor: e.titleBGColor,
+                discussion: e.discussion,
+                deletedAt: e.deletedAt,
+                createdAt: e.createdAt
+              };
+            }
+          });
+
+          res.json({ deletedThreads: deleted });
+        }
+      } else {
+        res.status(401);
+        res.json({ error: [errors.notAuthorized] });
       }
     } else {
-      res.status(401);
-      res.json({ error: [errors.notAuthorized] });
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
@@ -242,43 +314,54 @@ exports.getDeletedThreads = async (req, res, next) => {
 // make sure only the creator can edit a thread!
 exports.updateThread = async (req, res, next) => {
   const thread = await Thread.findById(req.params.threadId);
-  if (!thread) {
-    res.status(400);
-    res.json({ error: [errors.threadError] });
-  } else {
-    const threadAttr = thread.getAttributes(thread);
-    if (req.session.userId === threadAttr.UserId) {
-      const { title, discussion } = req.body;
-      await thread.update({
-        title: `Edited - ${title}`,
-        slug: slug(title),
-        discussion,
-        updatedAt: new Date()
-      });
-
-      await thread.reload();
-
-      const updatedThread = thread.getAttributes(thread);
-
-      res.json({
-        title: updatedThread.title,
-        slug: updatedThread.slug,
-        postCount: updatedThread.postCount,
-        locked: updatedThread.locked,
-        lockedReason: updatedThread.lockedReason,
-        lockedMessage: updatedThread.lockedMessage,
-        isSticky: updatedThread.isSticky,
-        stickyDuration: updatedThread.stickyDuration,
-        titleBGColor: updatedThread.titleBGColor,
-        discussion: updatedThread.discussion,
-        updatedAt: updatedThread.updatedAt
-      });
-    } else {
-      res.status(401);
-      res.json({ error: [errors.notAuthorized] });
-    }
-  }
   try {
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      if (!thread) {
+        res.status(400);
+        res.json({ error: [errors.threadError] });
+      } else {
+        const threadAttr = thread.getAttributes(thread);
+        if (req.session.userId === threadAttr.UserId) {
+          const { title, discussion } = req.body;
+          await thread.update({
+            title: `Edited - ${title}`,
+            slug: slug(title),
+            discussion,
+            updatedAt: new Date()
+          });
+
+          await thread.reload();
+
+          const updatedThread = thread.getAttributes(thread);
+
+          res.json({
+            title: updatedThread.title,
+            slug: updatedThread.slug,
+            postCount: updatedThread.postCount,
+            locked: updatedThread.locked,
+            lockedReason: updatedThread.lockedReason,
+            lockedMessage: updatedThread.lockedMessage,
+            isSticky: updatedThread.isSticky,
+            stickyDuration: updatedThread.stickyDuration,
+            titleBGColor: updatedThread.titleBGColor,
+            discussion: updatedThread.discussion,
+            updatedAt: updatedThread.updatedAt
+          });
+        } else {
+          res.status(401);
+          res.json({ error: [errors.notAuthorized] });
+        }
+      }
+    } else {
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
+    }
   } catch (error) {
     next(error);
   }
@@ -288,19 +371,30 @@ exports.updateThread = async (req, res, next) => {
 // make sure only a admin can do this!
 exports.deleteThread = async (req, res, next) => {
   try {
-    if (req.session.role !== "Admin") {
-      res.status(401);
-      res.json({ error: [errors.notAuthorized] });
-    } else {
-      const thread = await Thread.findById(req.params.threadId);
-
-      if (!thread) {
-        res.status(400);
-        res.json({ error: [errors.threadError] });
+    const token = req.cookies.token;
+    const decodedToken = jwtHelper.decodeJwt(token);
+    if (decodedToken.id === req.session.userId) {
+      if (req.session.role !== "Admin") {
+        res.status(401);
+        res.json({ error: [errors.notAuthorized] });
       } else {
-        await thread.destroy();
-        res.json({ success: true });
+        const thread = await Thread.findById(req.params.threadId);
+
+        if (!thread) {
+          res.status(400);
+          res.json({ error: [errors.threadError] });
+        } else {
+          await thread.destroy();
+          res.json({ success: true });
+        }
       }
+    } else {
+      // file a report on the user trying to do something out of the norm
+      // grab their ip address
+      // sign them out
+      // destroy any sessions
+      // send them to a warning page
+      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
