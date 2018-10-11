991af2a2689bdb93205983796c4f9b2d868f812d
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-12 10:36:35 +1100

    added friend controller

---
+++ controllers/friend/index.js
@@ -0,0 +1,21 @@
+// "/:userId/all";
+exports.getAllUserFriends = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/:userId/new"
+exports.addFriend = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/remove/:userId/remove"
+exports.removeFriend = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
