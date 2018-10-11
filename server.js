6b7d3edbc0a278998dd146f1263accf0e001572b
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-12 10:35:50 +1100

    updated with friend routes

--- server.js
+++ server.js
@@ -64,6 +64,7 @@ app.use("/api/v1/thread", require("./routes/thread"));
 app.use("/api/v1/post", require("./routes/post"));
 app.use("/api/v1/ban", require("./routes/ban"));
 app.use("/api/v1/like", require("./routes/like"));
+app.use("/api/v1/friend", require("./routes/friend"));
 
 // setup general error handler
 app.use(require("./helpers/generalErrors"));
