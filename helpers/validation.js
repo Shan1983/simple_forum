8211a30d9485d9f29325c580f083d5f7ab06bff7
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-14 01:04:38 +1100

    started moving all validation based methods this a central location

---
+++ helpers/validation.js
@@ -0,0 +1,12 @@
+exports.validateEmail = email => {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+exports.isEmpty = val => {
+  if (!val || val.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
