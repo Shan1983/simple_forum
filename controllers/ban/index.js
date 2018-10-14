9239dd99117b84e9492899668f9782afca14157e
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-15 08:20:06 +1100

    refactored ban controller, made more DRY

--- controllers/ban/index.js
+++ controllers/ban/index.js
@@ -1,43 +1,25 @@
 const { Ban, User } = require("../../models");
 const errors = require("../../helpers/mainErrors");
-const jwtHelper = require("../../helpers/jwtHelper");
+const attributes = require("../../helpers/getModelAttributes");
 
 exports.addBan = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
-    const decodedToken = jwtHelper.decodeJwt(token);
-
-    if (decodedToken.id === req.session.userId) {
-      if (req.session.role !== "Admin") {
-        res.status(401);
-        res.json({ error: [errors.notAuthorized] });
-      } else {
-        const user = await User.findById(req.body.userId);
-        if (!user) {
-          res.status(400);
-          res.json({ error: [errors.accountNotExists] });
-        } else {
-          const userAttributes = user.getAttributes(user);
-          if (Ban.checkIfBanned(userAttributes)) {
-            await Ban.banUser(req.body.userId, req.ip, req.body.reason);
-            // send email explaining the reason of the banning
-            // and their options of having the decision reversed..
-
-            res.json({ success: true });
-          } else {
-            res.status(400);
-            res.json({ error: [errors.banError] });
-          }
-        }
-      }
-    } else {
-      // file a report on the user trying to do something out of the norm
-      // grab their ip address
-      // sign them out
-      // destroy any sessions
-      // send them to a warning page
+    const user = await User.findById(req.body.userId);
+    if (!user) {
       res.status(400);
-      res.redirect("/views/warning/activity");
+      res.json({ error: [errors.accountNotExists] });
+    } else {
+      const userReq = attributes.convert(user);
+      if (Ban.checkIfBanned(userReq)) {
+        await Ban.banUser(req.body.userId, req.ip, req.body.reason);
+        // send email explaining the reason of the banning
+        // and their options of having the decision reversed..
+
+        res.json({ success: true });
+      } else {
+        res.status(400);
+        res.json({ error: [errors.banError] });
+      }
     }
   } catch (error) {
     next(errors);
@@ -45,44 +27,27 @@ exports.addBan = async (req, res, next) => {
 };
 exports.removeBan = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
-    const decodedToken = jwtHelper.decodeJwt(token);
-    if (decodedToken.id === req.session.userId) {
-      if (req.session.role !== "Admin") {
-        res.status(401);
-        res.json({ error: [errors.notAuthorized] });
-      } else {
-        const { email } = req.body;
+    const { email } = req.body;
 
-        const user = await User.findOne({
-          where: { email }
-        });
+    const user = await User.findOne({
+      where: { email }
+    });
 
-        if (!user) {
-          res.status(400);
-          res.json({ error: [errors.accountNotExists] });
-        } else {
-          const userAttributes = user.getAttributes(user);
-          if (Ban.checkIfBanned(userAttributes)) {
-            await Ban.unbanUser(userAttributes.id);
-
-            // send email explaining that the ban has been lifted
-
-            res.json({ success: true });
-          } else {
-            res.status(400);
-            res.json({ error: [errors.banError] });
-          }
-        }
-      }
-    } else {
-      // file a report on the user trying to do something out of the norm
-      // grab their ip address
-      // sign them out
-      // destroy any sessions
-      // send them to a warning page
+    if (!user) {
       res.status(400);
-      res.redirect("/views/warning/activity");
+      res.json({ error: [errors.accountNotExists] });
+    } else {
+      const userReq = attributes.convert(user);
+      if (Ban.checkIfBanned(userReq)) {
+        await Ban.unbanUser(userReq.id);
+
+        // send email explaining that the ban has been lifted
+
+        res.json({ success: true });
+      } else {
+        res.status(400);
+        res.json({ error: [errors.banError] });
+      }
     }
   } catch (error) {
     next(errors);
