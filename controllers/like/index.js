25950738ae4bdfe41e3888fb83cf0769ecaf1586
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-15 07:53:51 +1100

    refactored like controller to make it more DRY

--- controllers/like/index.js
+++ controllers/like/index.js
@@ -1,96 +1,87 @@
 const { Thread, Post, Like, User } = require("../../models");
 const errors = require("../../helpers/mainErrors");
-const jwtHelper = require("../../helpers/jwtHelper");
+const attributes = require("../../helpers/getModelAttributes");
 
 exports.addThreadLike = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const { threadId, postId } = req.body;
 
-    const decodedToken = jwtHelper.decodeJwt(token);
+    if (threadId) {
+      const thread = await Thread.findById(threadId);
 
-    if (decodedToken.id === req.session.userId) {
-      const { threadId, postId } = req.body;
+      if (!thread) {
+        res.status(400);
+        res.json({ error: [errors.threadError] });
+      } else {
+        const threadReq = attributes.convert(thread);
 
-      if (threadId) {
-        const thread = await Thread.findById(threadId);
+        if (threadReq.UserId !== req.session.userId) {
+          const like = await Like.findAndCountAll({
+            where: { ThreadId: attributes.id }
+          });
 
-        if (!thread) {
-          res.status(400);
-          res.json({ error: [errors.threadError] });
-        } else {
-          const attributes = thread.getAttributes(thread);
-
-          if (attributes.UserId !== req.session.userId) {
-            const like = await Like.findAndCountAll({
-              where: { ThreadId: attributes.id }
-            });
-
-            let error = false;
-            like.rows.map(l => {
-              if (l.UserId === req.session.userId) {
-                error = true;
-              }
-            });
-
-            if (!error) {
-              await Like.create({
-                UserId: req.session.userId,
-                ThreadId: attributes.id
-              });
-
-              res.json({ success: true, count: like.count + 1 });
-            } else {
-              res.status(400);
-              res.json({ error: [errors.invalidLike] });
+          let error = false;
+          like.rows.map(l => {
+            if (l.UserId === req.session.userId) {
+              error = true;
             }
+          });
+
+          if (!error) {
+            await Like.create({
+              UserId: req.session.userId,
+              ThreadId: threadReq.id
+            });
+
+            res.json({ success: true, count: like.count + 1 });
           } else {
             res.status(400);
             res.json({ error: [errors.invalidLike] });
           }
+        } else {
+          res.status(400);
+          res.json({ error: [errors.invalidLike] });
         }
       }
+    }
 
-      if (postId) {
-        const post = await Post.findById(postId);
+    if (postId) {
+      const post = await Post.findById(postId);
 
-        if (!post) {
-          res.status(400);
-          res.json({ error: [errors.postError] });
-        } else {
-          const attributes = post.getAttributes(post);
+      if (!post) {
+        res.status(400);
+        res.json({ error: [errors.postError] });
+      } else {
+        const postReq = attributes.convert(post);
 
-          if (attributes.UserId !== req.session.userId) {
-            const like = await Like.findAndCountAll({
-              where: { PostId: attributes.id }
-            });
+        if (postReq.UserId !== req.session.userId) {
+          const like = await Like.findAndCountAll({
+            where: { PostId: attributes.id }
+          });
 
-            let error = false;
-            like.rows.map(l => {
-              if (l.UserId === req.session.userId) {
-                error = true;
-              }
-            });
-
-            if (!error) {
-              await Like.create({
-                UserId: req.session.userId,
-                PostId: attributes.id
-              });
-
-              res.json({ success: true, count: like.count + 1 });
-            } else {
-              res.status(400);
-              res.json({ error: [errors.invalidLike] });
+          let error = false;
+          like.rows.map(l => {
+            if (l.UserId === req.session.userId) {
+              error = true;
             }
+          });
+
+          if (!error) {
+            await Like.create({
+              UserId: req.session.userId,
+              PostId: postReq.id
+            });
+
+            res.json({ success: true, count: like.count + 1 });
           } else {
             res.status(400);
             res.json({ error: [errors.invalidLike] });
           }
+        } else {
+          res.status(400);
+          res.json({ error: [errors.invalidLike] });
         }
       }
-    } else {
-      res.status(401);
-      res.json({ error: [errors.notAuthorized] });
     }
   } catch (error) {
     next(error);
@@ -99,57 +90,44 @@ exports.addThreadLike = async (req, res, next) => {
 
 exports.removeLike = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const { threadId, postId } = req.body;
 
-    const decodedToken = jwtHelper.decodeJwt(token);
+    if (threadId) {
+      const like = await Like.findAll({
+        where: { ThreadId: threadId, UserId: req.session.userId }
+      });
 
-    if (decodedToken.id === req.session.userId) {
-      const { threadId, postId } = req.body;
-
-      if (threadId) {
-        const like = await Like.findAll({
+      if (like.length <= 0) {
+        res.status(400);
+        res.json({ error: [errors.likeError] });
+      } else {
+        await Like.destroy({
           where: { ThreadId: threadId, UserId: req.session.userId }
         });
 
-        if (like.length <= 0) {
-          res.status(400);
-          res.json({ error: [errors.likeError] });
-        } else {
-          await Like.destroy({
-            where: { ThreadId: threadId, UserId: req.session.userId }
-          });
+        const count = await Like.count({ where: { ThreadId: threadId } });
 
-          const count = await Like.count({ where: { ThreadId: threadId } });
-
-          res.json({ success: true, count });
-        }
+        res.json({ success: true, count });
       }
+    }
 
-      if (postId) {
-        const like = await Like.findAll({
+    if (postId) {
+      const like = await Like.findAll({
+        where: { PostId: postId, UserId: req.session.userId }
+      });
+
+      if (like.length <= 0) {
+        res.status(400);
+        res.json({ error: [errors.likeError] });
+      } else {
+        await Like.destroy({
           where: { PostId: postId, UserId: req.session.userId }
         });
 
-        if (like.length <= 0) {
-          res.status(400);
-          res.json({ error: [errors.likeError] });
-        } else {
-          await Like.destroy({
-            where: { PostId: postId, UserId: req.session.userId }
-          });
+        const count = await Like.count({ where: { PostId: postId } });
 
-          const count = await Like.count({ where: { PostId: postId } });
-
-          res.json({ success: true, count });
-        }
+        res.json({ success: true, count });
       }
-    } else {
-      // file a report on the user trying to do something out of the norm
-      // grab their ip address
-      // sign them out
-      // destroy any sessions
-      // send them to a warning page
-      res.redirect("/views/warning/activity");
     }
   } catch (error) {
     next(error);
