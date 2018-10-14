e8b96722be3d926c22b8527d0a9e9e323df8551c
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-15 08:33:56 +1100

    refactored friend routes

--- routes/friend.js
+++ routes/friend.js
@@ -1,36 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
+const middleware = require("../services/middlewares/authMiddleware");
 
 const controller = require("../controllers/friend");
 
 router.get(
   "/:userId/all",
-  passport.authenticate("jwt", { session: false }),
+  middleware.isAuthenticated,
+  middleware.canContinue,
   controller.getAllUserFriends
 );
 
 router.post(
   "/:userId/accept",
-  passport.authenticate("jwt", { session: false }),
+  middleware.isAuthenticated,
+  middleware.canContinue,
   controller.accept
 );
 
 router.post(
   "/:userId/decline",
-  passport.authenticate("jwt", { session: false }),
+  middleware.isAuthenticated,
+  middleware.canContinue,
   controller.decline
 );
 
 router.post(
   "/:userId/new",
-  passport.authenticate("jwt", { session: false }),
+  middleware.isAuthenticated,
+  middleware.canContinue,
   controller.addFriend
 );
 
 router.delete(
   "/:fromId/:toId/remove",
-  passport.authenticate("jwt", { session: false }),
+  middleware.isAuthenticated,
+  middleware.canContinue,
   controller.removeFriend
 );
 
