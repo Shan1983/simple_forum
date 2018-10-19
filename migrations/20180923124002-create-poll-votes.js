7fa7174f06a96b0d5bd561f7cbc28c3f0590d2bf
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-19 15:21:51 +1100

    renamed migration

---
+++ migrations/20180923124002-create-poll-votes.js
@@ -0,0 +1,39 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      "pollVotes",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        UserId: {
+          type: Sequelize.INTEGER
+        },
+        PollQuestionId: {
+          type: Sequelize.INTEGER
+        },
+        PollResponseId: {
+          type: Sequelize.INTEGER
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      },
+      {
+        charset: "utf8mb4"
+      }
+    );
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("pollVotes");
+  }
+};
