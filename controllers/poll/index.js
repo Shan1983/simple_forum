49adc71cdeefaae0e6d1a7de9a049693ac3a1221
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-19 11:03:36 +1100

    added poll controller

---
+++ controllers/poll/index.js
@@ -0,0 +1,46 @@
+const errors = require("../../helpers/mainErrors");
+const { PollBallot, PollQuery, PollResponse } = require("../../models");
+const attributes = require("../../helpers/getModelAttributes");
+
+// /:pollId
+exports.getAPoll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// /all
+exports.getAllPolls = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// /new
+exports.newPoll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// /:pollId/questions
+exports.newPollQuestions = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// /:pollId
+exports.editPoll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// /:pollId/remove
+exports.removePoll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
