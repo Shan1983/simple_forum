64647be51b4feb0f268146b7f4fc764ae53f7efe
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-21 00:43:46 +1100

    completed blacklist functionalitly

--- controllers/blacklist/index.js
+++ controllers/blacklist/index.js
@@ -1,10 +1,19 @@
-const { User, Blacklist } = require("../../models");
+const { Blacklist } = require("../../models");
 const errors = require("../../helpers/mainErrors");
 const attributes = require("../../helpers/getModelAttributes");
+const validate = require("../../helpers/validation");
 
 // "/";
 exports.getBlacklist = async (req, res, next) => {
   try {
+    // grab everyone in the blacklist
+    const blacklist = await Blacklist.findAndCountAll();
+    if (blacklist.length <= 0) {
+      res.status(400);
+      res.json({ error: [errors.blacklistError] });
+    } else {
+      res.json(blacklist);
+    }
   } catch (error) {
     next(error);
   }
@@ -12,6 +21,26 @@ exports.getBlacklist = async (req, res, next) => {
 // "/";
 exports.addToBlacklist = async (req, res, next) => {
   try {
+    const { playerTag, currentName, reason } = req.body;
+
+    // validate the inputs
+    if (
+      validate.isEmpty(playerTag) &&
+      validate.isEmpty(currentName) &&
+      validate.isEmpty(reason)
+    ) {
+      res.status(400);
+      res.json({ error: [errors.blacklistValidationError] });
+    } else {
+      // add a new person to the black list
+      await Blacklist.create({
+        playerTag,
+        currentName,
+        reason
+      });
+
+      res.json({ success: true });
+    }
   } catch (error) {
     next(error);
   }
@@ -19,6 +48,23 @@ exports.addToBlacklist = async (req, res, next) => {
 // "/:id";
 exports.removeFromBlacklist = async (req, res, next) => {
   try {
+    const { playerTag } = req.body;
+
+    // grab a user in the list
+    const user = await Blacklist.findOne({
+      where: { playerTag }
+    });
+
+    if (!user) {
+      res.status(400);
+      res.json({ error: [errors.accountNotExists] });
+    } else {
+      const userReq = attributes.convert(user);
+      // remove them from the list - maybe discuss this with everyone first..
+      await Blacklist.destroy({ where: { id: userReq.id } });
+
+      res.json({ success: true });
+    }
   } catch (error) {
     next(error);
   }
