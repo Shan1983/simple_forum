d2b2c40682fea1c3e73f06193c98565acef98263
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-21 00:12:13 +1100

    started blacklist controller

---
+++ controllers/blacklist/index.js
@@ -0,0 +1,25 @@
+const { User, Blacklist } = require("../../models");
+const errors = require("../../helpers/mainErrors");
+const attributes = require("../../helpers/getModelAttributes");
+
+// "/";
+exports.getBlacklist = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/";
+exports.addToBlacklist = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/:id";
+exports.removeFromBlacklist = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
