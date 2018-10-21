f36c2b703b3fb673cdab4bd0d6db83a25515250a
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-22 00:25:46 +1100

    added stubs for controller

---
+++ controllers/penaltybox/index.js
@@ -0,0 +1,32 @@
+const { PenaltyBox, User } = require("../../models");
+const errors = require("../../helpers/mainErrors");
+const attributes = require("../../helpers/getModelAttributes");
+
+// "/"
+exports.getAllInBox = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/:userId"
+exports.getAUserInBox = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/:userId"
+exports.putAUserInTheBox = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/:userId"
+exports.removeAUserFromBox = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
