76c54d20031cdb2ebfe243955234c23b527b45d3
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-22 00:14:48 +1100

    removed user association, is standalone from other models

--- models/blacklist.js
+++ models/blacklist.js
@@ -13,31 +13,5 @@ module.exports = (sequelize, DataTypes) => {
 
   // class methods
 
-  Blacklist.associate = function(models) {
-    Blacklist.belongsTo(models.User);
-  };
-
-  Blacklist.addUser = function(tag, name, reason) {
-    Blacklist.create({
-      tag,
-      currentName: name,
-      reason,
-      previousName: name
-    });
-  };
-
-  // instance methods
-  Blacklist.prototype.removeUser = async function(name) {
-    const { Blacklist } = sequelize.models;
-
-    const bl = await Blacklist.findOne({
-      where: { currentName: name }
-    });
-
-    bl.destroy();
-
-    return true;
-  };
-
   return Blacklist;
 };
