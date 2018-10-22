127e393c176378e5d87a2325ee616ae6f9e4ccc4
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-22 23:01:21 +1100

    added reporting test cases

---
+++ test/cases/report.test.js
@@ -0,0 +1,10 @@
+"use strict";
+process.env.NODE_ENV = "test";
+const server = require("../../server");
+const chai = require("chai");
+const should = chai.should();
+const expect = chai.expect;
+const errors = require("../../helpers/mainErrors");
+
+chai.use(require("chai-http"));
+chai.use(require("chai-things"));
