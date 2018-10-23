371e79efed7425b7203c4eae08ef1cd5a1bc9f7d
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-24 00:24:01 +1100

    added setting stubs

---
+++ controllers/setting/index.js
@@ -0,0 +1,133 @@
+// "/";
+exports.getAllSettings = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/clan-shield"
+exports.getClanShield = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/forum-name"
+exports.postForumName = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/forum-description"
+exports.postForumDescription = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/show-description"
+exports.postShowDescription = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/show-clan-size"
+exports.postShowClanSize = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/show-blacklist"
+exports.postShowBlacklist = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/show-clan-shield"
+exports.postShowClanShield = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/maintenance"
+exports.postMaintenance = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/lock-forum"
+exports.postLockForum = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/allow-best-post"
+exports.postAllowBestPost = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/allow-email-subscriptions"
+exports.postAllowEmailSubs = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/repost-time"
+exports.postReportTime = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/allow-likes"
+exports.postAllowLikes = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/editor"
+exports.postEditor = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/set-admin
+exports.postSetAdmin = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/set-word-limit"
+exports.postSetWordLimit = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/allow-subscriptions"
+exports.postAllowSubs = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+// "/allow-sticky-posts"
+exports.postAllowStickyPosts = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
