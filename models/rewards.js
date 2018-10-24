01e47d559283920df48781132089a053d0cf0cbf
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-24 22:53:33 +1100

    reflected migration change

--- models/rewards.js
+++ models/rewards.js
@@ -3,18 +3,33 @@ module.exports = (sequelize, DataTypes) => {
   const Rewards = sequelize.define(
     "Rewards",
     {
+      init: { type: DataTypes.BOOLEAN, defaultValue: false },
       pointsPerPost: DataTypes.INTEGER,
       pointsPerThread: DataTypes.INTEGER,
       pointsPerLike: DataTypes.INTEGER,
       pointsPerBestPost: DataTypes.INTEGER,
       pointsForAdvertising: DataTypes.INTEGER,
-      pointsPerPollQuery: DataTypes.INTEGER,
-      pointsPerPollBallot: DataTypes.INTEGER
+      pointsPerPollQuestion: DataTypes.INTEGER,
+      pointsPerPollVote: DataTypes.INTEGER
     },
     {}
   );
   // Rewards.associate = function(models) {
   //   // associations can be defined here
   // };
+
+  Rewards.initialSetup = async function() {
+    await Rewards.create({
+      pointsPerPost: 20,
+      pointsPerThread: 20,
+      pointsPerLike: 20,
+      pointsPerBestPost: 50,
+      pointsForAdvertising: 1000,
+      pointsPerPollQuestion: 20,
+      pointsPerPollVote: 20,
+      init: true
+    });
+  };
+
   return Rewards;
 };
