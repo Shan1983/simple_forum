cce049332d8584bb9f19b1bb4a539753359f2e1b
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-25 01:15:04 +1100

    changed server for maintenance

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ app.use("/api/v1/blacklist", require("./routes/blacklist"));
 app.use("/api/v1/penaltybox", require("./routes/penaltybox"));
 app.use("/api/v1/report", require("./routes/report"));
 app.use("/api/v1/setting", require("./routes/setting"));
+app.use("/api/v1/reward", require("./routes/reward"));
 
 // setup general error handler
 app.use(require("./helpers/generalErrors"));
