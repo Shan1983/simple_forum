9541df5c1e7155d490f807d60e51fd50668c5265
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-25 01:13:11 +1100

    updated route with maintenance and lock check

--- routes/friend.js
+++ routes/friend.js
@@ -4,6 +4,15 @@ const middleware = require("../services/middlewares/authMiddleware");
 
 const controller = require("../controllers/friend");
 
+router.all("*", (req, res, next) => {
+  if (req.app.locals.maintenance || req.app.locals.lockForum) {
+    res.status(503);
+    res.json({ status: 503 });
+  } else {
+    next();
+  }
+});
+
 router.get(
   "/:userId/all",
   middleware.isAuthenticated,
