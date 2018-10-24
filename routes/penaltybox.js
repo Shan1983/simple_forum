329eb21dc6f8e78120874c3e5f6c16acbc990125
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-25 01:13:31 +1100

    updated route with maintenance and lock check

--- routes/penaltybox.js
+++ routes/penaltybox.js
@@ -4,6 +4,15 @@ const router = express.Router();
 const middleware = require("../services/middlewares/authMiddleware");
 const controller = require("../controllers/penaltybox");
 
+router.all("*", (req, res, next) => {
+  if (req.app.locals.maintenance || req.app.locals.lockForum) {
+    res.status(503);
+    res.json({ status: 503 });
+  } else {
+    next();
+  }
+});
+
 router.get(
   "/",
   middleware.isAuthenticated,
