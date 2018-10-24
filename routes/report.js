931f88270d469b761ccb9528e2a351b1aa65797e
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-25 01:14:06 +1100

    updated route with maintenance and lock check

--- routes/report.js
+++ routes/report.js
@@ -4,6 +4,15 @@ const router = express.Router();
 const controller = require("../controllers/report");
 const middleware = require("../services/middlewares/authMiddleware");
 
+router.all("*", (req, res, next) => {
+  if (req.app.locals.maintenance || req.app.locals.lockForum) {
+    res.status(503);
+    res.json({ status: 503 });
+  } else {
+    next();
+  }
+});
+
 router.get(
   "/",
   middleware.isAuthenticated,
