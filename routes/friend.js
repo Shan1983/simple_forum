4f0035ac1676c3426fbd879b4b9a173ee9eabbdf
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-27 21:29:00 +1100

    added logger

--- routes/friend.js
+++ routes/friend.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const middleware = require("../services/middlewares/authMiddleware");
 
 const controller = require("../controllers/friend");
+const logger = require("../services/middlewares/logger");
 
 router.all("*", (req, res, next) => {
   if (req.app.locals.maintenance || req.app.locals.lockForum) {
@@ -17,6 +18,7 @@ router.get(
   "/:userId/all",
   middleware.isAuthenticated,
   middleware.canContinue,
+  logger.general,
   controller.getAllUserFriends
 );
 
@@ -24,6 +26,7 @@ router.post(
   "/:userId/accept",
   middleware.isAuthenticated,
   middleware.canContinue,
+  logger.general,
   controller.accept
 );
 
@@ -31,6 +34,7 @@ router.post(
   "/:userId/decline",
   middleware.isAuthenticated,
   middleware.canContinue,
+  logger.general,
   controller.decline
 );
 
@@ -38,6 +42,7 @@ router.post(
   "/:userId/new",
   middleware.isAuthenticated,
   middleware.canContinue,
+  logger.general,
   controller.addFriend
 );
 
@@ -45,6 +50,7 @@ router.delete(
   "/:fromId/:toId/remove",
   middleware.isAuthenticated,
   middleware.canContinue,
+  logger.general,
   controller.removeFriend
 );
 
