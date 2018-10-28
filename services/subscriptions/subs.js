a5c6c5b14faf7c6f17408c74274127ecf8d4e61a
Shannan Galbraith <s.galbraith08@gmail.com>
2018-10-29 01:28:29 +1100

    added subscriptions for threads

--- services/subscriptions/subs.js
+++ services/subscriptions/subs.js
@@ -1,22 +1,28 @@
-const { Subscription, Thread } = require("../../models");
+const { Subscription, Thread, User } = require("../../models");
 const attributes = require("../../helpers/getModelAttributes");
 const emailer = require("../emails/send");
 
 // on each new forum post check the subscription list and send the new post
 exports.checkSubsAndSendNewMessage = async data => {
   const subs = await Subscription.findAll({ where: { sendEmails: true } });
-  const thread = await Thread.findById(data.threadId);
-  const threadReq = await attributes.convert(thread);
-  // send the new post to everyone subscribed
-  subs.map(async () => {
-    const subObj = {
-      template: "subscription",
-      name: data.name,
-      siteName: process.env.SITE_NAME,
-      thread: threadReq.title,
-      discussion: data.post
-    };
 
+  // send the new post to everyone subscribed
+
+  let subObj = {};
+  subs.map(async s => {
+    const thread = await Thread.findById(s.ThreadId);
+    const user = await User.findById(s.UserId);
+    const threadReq = await attributes.convert(thread);
+    const userReq = await attributes.convert(user);
+
+    subObj = {
+      template: "subscription",
+      to: userReq.email,
+      name: userReq.username,
+      siteName: process.env.SITE_NAME,
+      threadTitle: threadReq.title,
+      discussion: data
+    };
     // send the email
     emailer.sendEmail(subObj);
   });
